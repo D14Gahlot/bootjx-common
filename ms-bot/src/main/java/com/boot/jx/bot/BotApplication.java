465b7cb2b57beb7923d0d1ab8adcd843da571b29
Lalit tanwar <lalit.tanwar07@gmail.com>
2021-01-02 08:17:11 +0530

    certs added

--- ms-bot/src/main/java/com/boot/jx/bot/BotApplication.java
+++ ms-bot/src/main/java/com/boot/jx/bot/BotApplication.java
@@ -18,18 +18,28 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @ComponentScan(value = "com.boot.jx")
 @EnableTransactionManagement
-@EnableAutoConfiguration(exclude = {
-		JmxAutoConfiguration.class, WebSocketAutoConfiguration.class, SitePreferenceAutoConfiguration.class,
-		SpringApplicationAdminJmxAutoConfiguration.class, ValidationAutoConfiguration.class
-})
+@EnableAutoConfiguration(exclude = { JmxAutoConfiguration.class, WebSocketAutoConfiguration.class,
+		SitePreferenceAutoConfiguration.class, SpringApplicationAdminJmxAutoConfiguration.class,
+		ValidationAutoConfiguration.class })
 @EnableAsync(proxyTargetClass = true)
 public class BotApplication {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(BotApplication.class);
 
 	public static void main(String[] args) throws Exception {
-		ConfigurableApplicationContext ctx = new SpringApplicationBuilder(BotApplication.class)
-				.run(args);
+
+		String javaHomePath = System.getProperty("java.home");
+		String keystore = javaHomePath + "/lib/security/cacerts";
+		String storepass = "changeit";
+		String storetype = "JKS";
+
+		String[][] props = { { "javax.net.ssl.trustStore", keystore, }, { "javax.net.ssl.keyStore", keystore, },
+				{ "javax.net.ssl.keyStorePassword", storepass, }, { "javax.net.ssl.keyStoreType", storetype, }, };
+		for (int i = 0; i < props.length; i++) {
+			System.getProperties().setProperty(props[i][0], props[i][1]);
+		}
+
+		ConfigurableApplicationContext ctx = new SpringApplicationBuilder(BotApplication.class).run(args);
 	}
 
 }
