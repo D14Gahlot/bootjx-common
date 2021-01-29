b93a0682a7a09a85f896290e95ba7da034e8df33
Lalit tanwar <lalit.tanwar07@gmail.com>
2021-01-30 03:18:30 +0530

    categories

--- server-agent/src/main/resources/static/assets/js/dashboard.agent2.js
+++ server-agent/src/main/resources/static/assets/js/dashboard.agent2.js
@@ -333,6 +333,10 @@ var WhatsApp = function ToDoView(app) {
     	}
     	$tags.data("mid",app.hisLastMessage.m.messageId);
     	$tags.empty();
+
+    	if(!app.hisLastMessage.m && !app.hisLastMessage.m.tags && !app.hisLastMessage.m.tags.categories){
+    		return;
+    	}
         $.getJSON("/agent/category/map/smart_reply.json?value="+
         		(app.hisLastMessage.m.tags || {categories : []}).categories.join(",")
         		, function (data) {
