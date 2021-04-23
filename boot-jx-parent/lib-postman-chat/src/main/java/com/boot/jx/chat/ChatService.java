2d4c7c2450fe6054dfba2bc10b9cc9128c8602af
Lalit tanwar <lalit.tanwar07@gmail.com>
2021-04-23 18:49:26 +0530

    connector added

--- boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/chat/ChatService.java
+++ boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/chat/ChatService.java
@@ -279,6 +279,7 @@ public class ChatService {
 				chatUserProfileRequest.setContactId(contact.getContactId());
 				ChatUserProfileDTO profile = chatClient.fetchContactDetails(chatUserProfileRequest);
 				contact.setProfile(profile);
+				sessionStore.save(contact);
 			} catch (Exception e) {
 
 			}
