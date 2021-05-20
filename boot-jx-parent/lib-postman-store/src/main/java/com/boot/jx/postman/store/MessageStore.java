37ee394705c9fee64ff7681101b0ac293aa3a59a
Lalit tanwar <lalit.tanwar07@gmail.com>
2021-05-20 15:07:56 +0530

    added reason to it

--- boot-jx-parent/lib-postman-store/src/main/java/com/boot/jx/postman/store/MessageStore.java
+++ boot-jx-parent/lib-postman-store/src/main/java/com/boot/jx/postman/store/MessageStore.java
@@ -233,9 +233,14 @@ public class MessageStore extends CommonDocStore {
 		if (ArgUtil.is(messageReport.getStatus())) {
 			builder.set("status", messageReport.getStatus());
 			builder.set("stampss." + messageReport.getStatus().toString(), messageReport.getTimestamp());
+
+			if (ArgUtil.is(messageReport)) {
+				builder.getUpdate().push("logs", messageReport.getReason());
+			}
+
 			mongoTemplate.updateFirst(builder.getQuery(), builder.getUpdate(), MessageDoc.class,
 					getCollectionName(messageReport.getContactType()));
-			//LOGGER.info(JsonUtil.toJson(builder));
+			// LOGGER.info(JsonUtil.toJson(builder));
 		}
 	}
 
