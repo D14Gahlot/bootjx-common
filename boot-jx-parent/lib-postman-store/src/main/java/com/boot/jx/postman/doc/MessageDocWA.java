544ffb8ff7576c8928ffddbf0b40b46ab0666783
Lalit tanwar <lalit.tanwar07@gmail.com>
2021-06-09 06:04:16 +0530

    Added Message Doc

--- boot-jx-parent/lib-postman-store/src/main/java/com/boot/jx/postman/doc/MessageDocWA.java
+++ boot-jx-parent/lib-postman-store/src/main/java/com/boot/jx/postman/doc/MessageDocWA.java
@@ -4,9 +4,9 @@ import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = MessageDocWA.COLLECTION_NAME + "_WHATSAPP")
-@TypeAlias("MessageDoc")
+@TypeAlias("MessageDocWA")
 public class MessageDocWA extends MessageDoc {
 
 	private static final long serialVersionUID = -2175867087327783840L;
-	
+
 }
