7b8e2a6737cbc40f2be27fac400c14f65394ffc3
Lalit Tanwar <lalittanwar@Lalit-ka-MacBook-Pro.local>
2021-07-30 10:42:08 +0530

    script updated

--- scripts/sync.master_production.sh
+++ scripts/sync.master_production.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-git co production && git up && git merge master && git up && git pu && git co master && git merge production
+git up && git co production && git up && git merge master && git up && git pu && git co master && git merge production
