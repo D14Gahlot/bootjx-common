a66438a4b499ce33eb309285217c104e9927cdad
lalit.tanwar <lalit.tanwar07@gmail.com>
2021-10-01 00:29:13 +0530

    Added addtional params

--- server-account/src/main/java/com/boot/jx/account/api/PartnerController.java
+++ server-account/src/main/java/com/boot/jx/account/api/PartnerController.java
@@ -234,6 +234,10 @@ public class PartnerController {
 	if (!ArgUtil.is(domainDoc.getCompany().getConactEmail())) {
 	    domainDoc.getCompany().setConactEmail(domainUser.getContact().getEmail());
 	}
+	
+	if (!ArgUtil.is(domainDoc.getCompany().getConactPhone())) {
+	    domainDoc.getCompany().setConactPhone(domainUser.getContact().getPhone());
+	}
 
 	if (!ArgUtil.is(domainDoc.getCompany().getBusinessName())) {
 	    domainDoc.getCompany().setBusinessName(domainUser.getContact().getCompany());
