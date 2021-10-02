fe8bc8ca368e01db2d8e902e3273606cfc5017de
lalit.tanwar <lalit.tanwar07@gmail.com>
2021-10-02 21:07:51 +0530

    inbound

--- server-xms/src/main/java/com/boot/jx/xms/dto/OutBoundReciept.java
+++ server-xms/src/main/java/com/boot/jx/xms/dto/OutBoundReciept.java
@@ -1,6 +1,7 @@
 package com.boot.jx.xms.dto;
 
 import com.boot.jx.swagger.ApiMockModelProperty;
+import com.boot.model.UtilityModels.JsonIgnoreUnknown;
 
 /**
  * https://developers.facebook.com/docs/whatsapp/api/errors
@@ -8,8 +9,22 @@ import com.boot.jx.swagger.ApiMockModelProperty;
  * @author lalittanwar
  *
  */
-public class OutBoundReciept {
+public class OutBoundReciept implements JsonIgnoreUnknown {
 
+    private static final long serialVersionUID = 6528883942938374590L;
     @ApiMockModelProperty(example = "gBEGkYiEB1VXAglK1ZEqA1YKPrU")
     public String id;
+
+    public String getId() {
+	return id;
+    }
+
+    public void setId(String id) {
+	this.id = id;
+    }
+
+    public OutBoundReciept id(String id) {
+	this.id = id;
+	return this;
+    }
 }
