4e49ae0d28e315ce735b5f3283c4ca89ce03b263
lalit.tanwar <lalit.tanwar07@gmail.com>
2021-10-08 00:10:47 +0530

    msg fix

--- boot-jx-parent/lib-postman-store/src/main/java/com/boot/jx/postman/store/SessionStore.java
+++ boot-jx-parent/lib-postman-store/src/main/java/com/boot/jx/postman/store/SessionStore.java
@@ -506,23 +506,22 @@ public class SessionStore extends CommonDocStore {
 
     }
 
-    public void push(MessageDoc msgDoc, IMessage outboxMessage) {
-	ChatSessionQuery chatSessionDocQuery = new ChatSessionQuery(msgDoc.getSessionId());
-	if (PostManUtil.isInBound(msgDoc.getType())) {
-	    chatSessionDocQuery.setLastInBoundMsg(msgDoc, outboxMessage.contact().getContactType());
-	    commonMongoTemplate.updateFirst(chatSessionDocQuery);
-	} else if (PostManUtil.isOutBound(msgDoc.getType())) {
-	    if (PostManUtil.isAgentMode(outboxMessage)) {
-		chatSessionDocQuery.setLastAgentReply(msgDoc, outboxMessage.session().getMode());
-	    } else if (PostManUtil.isBotMode(outboxMessage)) {
-		chatSessionDocQuery.setLastBotReply(msgDoc, outboxMessage.contact().getContactType());
-	    } else {
-		chatSessionDocQuery.setLastOutBoundMsg(msgDoc, outboxMessage.contact().getContactType());
-	    }
-	    commonMongoTemplate.updateFirst(chatSessionDocQuery);
-	}
+    public void push(MessageDoc msgDoc, IMessage iMessage) {
 	if (PostManUtil.isInBound(msgDoc.getType()) || PostManUtil.isOutBound(msgDoc.getType())) {
-	    chatSessionDocQuery.setLastAgentReply(msgDoc, outboxMessage.contact().getContactType());
+	    ChatSessionQuery chatSessionDocQuery = new ChatSessionQuery(msgDoc.getSessionId());
+	    if (PostManUtil.isInBound(msgDoc.getType())) {
+		chatSessionDocQuery.setLastInBoundMsg(msgDoc, iMessage.contact().getContactType());
+		commonMongoTemplate.updateFirst(chatSessionDocQuery);
+	    } else if (PostManUtil.isOutBound(msgDoc.getType())) {
+		if (PostManUtil.isAgentMode(iMessage)) {
+		    chatSessionDocQuery.setLastAgentReply(msgDoc, iMessage.session().getMode());
+		} else if (PostManUtil.isBotMode(iMessage)) {
+		    chatSessionDocQuery.setLastBotReply(msgDoc, iMessage.contact().getContactType());
+		} else {
+		    chatSessionDocQuery.setLastOutBoundMsg(msgDoc, iMessage.contact().getContactType());
+		}
+	    }
+	    chatSessionDocQuery.setLastMsg(msgDoc, iMessage.contact().getContactType());
 	    commonMongoTemplate.updateFirst(chatSessionDocQuery);
 	}
 
