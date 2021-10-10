667ddb78230aaad4474faead55a7980790f1468e
lalit.tanwar <lalit.tanwar07@gmail.com>
2021-10-10 06:20:26 +0530

    updated script

--- scripts/build.prod.sh
+++ scripts/build.prod.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-git co production && git up && git pu && git co "build-prod-${1}" && git up && git pu && git merge production && git pu && git co master
+git co production && git up && git pu && git co "build-prod-${1}" && git up && git pu && git merge production && git pu && git co production
