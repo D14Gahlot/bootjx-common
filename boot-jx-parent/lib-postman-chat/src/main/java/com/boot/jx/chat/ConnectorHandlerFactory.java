b74aeb068c402e734cad38d9a0ca0aee22d5557b
lalit.tanwar <lalit.tanwar07@gmail.com>
2021-10-15 04:41:32 +0530

    Added Conatc Check

--- boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/chat/ConnectorHandlerFactory.java
+++ boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/chat/ConnectorHandlerFactory.java
@@ -267,11 +267,15 @@ public class ConnectorHandlerFactory extends ScopedBeanFactory<String, Connector
 	ChannelConfig channelConfig = environment.config().channels(channelId);
 
 	try {
-	    ConnectorHandler connector = get(channelConfig);
-	    if (ArgUtil.is(connector)) {
-		connector.message(messageType, chatContactDoc, inboxMessage, outboxMessage);
+	    if (ArgUtil.is(channelConfig) || ContactType.WEBSITE.equals(outboxMessage.contact().type())) {
+		ConnectorHandler connector = get(channelConfig);
+		if (ArgUtil.is(connector)) {
+		    connector.message(messageType, chatContactDoc, inboxMessage, outboxMessage);
+		} else {
+		    outboxMessage.logs().add(String.format("Connector not defined for %s", channelId));
+		}
 	    } else {
-		outboxMessage.logs().add(String.format("Connector not defined for %s", channelId));
+		outboxMessage.logs().add(String.format("ChannelConfig not found for %s", channelId));
 	    }
 
 	} catch (Exception e) {
