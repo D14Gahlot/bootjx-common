871cbae5bcca3844afe6f8d6b021570666a687fb
rabiluddin <rabiluddin@gmail.com>
2021-10-17 22:50:17 +0300

    msg

--- ms-bot/src/main/java/com/boot/jx/bot/demo/Demo1Controller.java
+++ ms-bot/src/main/java/com/boot/jx/bot/demo/Demo1Controller.java
@@ -271,7 +271,7 @@ public class Demo1Controller extends ChatController {
 	    InboxMessage agentAssignResp = assignToAgent().getResult();
 	    if (ArgUtil.is(agentAssignResp.session().getAgent())) {
 	    	if(ArgUtil.is(AppContextUtil.getTenant()) && AppContextUtil.getTenant().equalsIgnoreCase("tathkarah")) {
-	    	reply("، عميلنا العزيز،\r\n"
+	    	reply("، عميلنا العزيز\r\n"
 	    			+ "\r\n"
 	    			+ "مرحباً بك في تطبيق تذكره!\r\n"
 	    			+ "\r\n"
