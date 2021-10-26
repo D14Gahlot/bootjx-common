8815e1ba861a56c880bf006c66c43e367d9340bf
lalit.tanwar <lalit.tanwar07@gmail.com>
2021-10-26 22:46:48 +0530

    duplicate config removed

--- lib-common/lib-common-config/src/main/java/com/boot/jx/common/config/ConfigConstants.java
+++ lib-common/lib-common-config/src/main/java/com/boot/jx/common/config/ConfigConstants.java
@@ -22,7 +22,7 @@ public class ConfigConstants {
 
     public static enum KEY implements EntryMeta {
 	POSTMAN_BOT_NAME(new ConfigMeta("Bot Name", "postman.bot.name")),
-	POSTMAN_CONTACT_DETAILS_URL(new ConfigMeta("Bot Name", "postman.bot.name")),
+	POSTMAN_CONTACT_DETAILS_URL(new ConfigMeta("Contact Details Provider Webhook", "postman.contact.details.url")),
 
 	CHAT_TAG_ENABLED(new ConfigMeta("Chat Tag Enabled", "chat.tag.enabled").optionsOnOff()),
 
