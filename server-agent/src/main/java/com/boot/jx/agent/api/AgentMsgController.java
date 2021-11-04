9f41dbecaa9cd38d13e86e4481801de5ee6eb228
lalit.tanwar <lalit.tanwar07@gmail.com>
2021-11-05 03:31:48 +0530

    app vslue

--- server-agent/src/main/java/com/boot/jx/agent/api/AgentMsgController.java
+++ server-agent/src/main/java/com/boot/jx/agent/api/AgentMsgController.java
@@ -71,7 +71,7 @@ public class AgentMsgController {
 	List<ChatSessionDTO> chatSessionDtos = new ArrayList<ChatSessionDTO>();
 	if (agentSession.isLoggedIn() && ArgUtil.is(agentSession.getAgentDept())) {
 	    List<ChatSessionDoc> sessions = null;
-	    long historyPeriod = environment.keyEntry(KEY.POSTMAN_AGENT_TAB_HISTORY_PERIOD).asLong();
+	    long historyPeriod = environment.keyEntry(KEY.POSTMAN_AGENT_TAB_HISTORY_PERIOD).asLong(0L);
 	    if (historyPeriod > 0L) {
 		sessions = sessionStore.findChatSessionDocByAgentAndUnAssigned(agentSession.getAgentCode(),
 			agentSession.getAgentDept(),
