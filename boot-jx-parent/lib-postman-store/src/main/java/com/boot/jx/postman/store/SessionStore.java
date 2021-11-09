72e684e6ac87e79212a0aa3fb7d61898224de92d
lalit.tanwar <lalit.tanwar07@gmail.com>
2021-11-09 22:25:36 +0530

    added stampts

--- boot-jx-parent/lib-postman-store/src/main/java/com/boot/jx/postman/store/SessionStore.java
+++ boot-jx-parent/lib-postman-store/src/main/java/com/boot/jx/postman/store/SessionStore.java
@@ -356,7 +356,7 @@ public class SessionStore extends CommonDocStore {
 		DEFAULT_VALUES.POSTMAN_AGENT_TAB_HISTORY_PERIOD);
     }
 
-    public List<ChatSessionDoc> findSimilarChatSessionForContactId(String contactId) {
+    public List<ChatSessionDoc> findSimilarChatSessionForContactId(String contactId, Long fromStamp, Long toStamp) {
 	ChatContactDoc contact = getContact(contactId);
 
 	List<ChatContactDoc> contacts = null;
@@ -377,8 +377,8 @@ public class SessionStore extends CommonDocStore {
 	}
 
 	Calendar timeout = Calendar.getInstance();
-	timeout.setTimeInMillis(timeout.getTimeInMillis() - DEFAULT_VALUES.POSTMAN_AGENT_TAB_HISTORY_PERIOD*30);
-	
+	timeout.setTimeInMillis(timeout.getTimeInMillis() - DEFAULT_VALUES.POSTMAN_AGENT_TAB_HISTORY_PERIOD * 30);
+
 	Query query2 = new Query();
 	List<Criteria> orExpression = new ArrayList<Criteria>();
 
@@ -389,8 +389,16 @@ public class SessionStore extends CommonDocStore {
 	} else {
 	    orExpression.add(Criteria.where("contactId").is(contactId));
 	}
-	query2.addCriteria(Criteria.where("updatedStamp").gte(timeout.getTimeInMillis())
-		.orOperator(orExpression.toArray(new Criteria[orExpression.size()])));
+	//Time Limit Criteria
+	Criteria tymCriteria = Criteria.where("updatedStamp");
+	if (fromStamp > 0L) {
+	    tymCriteria.gte(fromStamp);
+	}
+	if (toStamp > 0L) {
+	    tymCriteria.lt(toStamp);
+	}
+
+	query2.addCriteria(tymCriteria.orOperator(orExpression.toArray(new Criteria[orExpression.size()])));
 	// LOGGER.info(query2.toString());
 	query2.fields().exclude("lastInBoundMsg").exclude("lastBotReply").exclude("lastAgentReply")
 		.exclude("lastOutBoundMsg").exclude("lastMsg");
