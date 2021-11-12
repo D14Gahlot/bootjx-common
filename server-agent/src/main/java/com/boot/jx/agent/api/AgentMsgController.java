58e7bc7d0a872b44fe5ee2845d22bac42108e5cc
lalit.tanwar <lalit.tanwar07@gmail.com>
2021-11-12 18:17:06 +0530

    any one can comment

--- server-agent/src/main/java/com/boot/jx/agent/api/AgentMsgController.java
+++ server-agent/src/main/java/com/boot/jx/agent/api/AgentMsgController.java
@@ -119,7 +119,9 @@ public class AgentMsgController {
 	ChatSessionDoc sessionDoc = sessionStore.getSession(outboxMessage.getSessionId());
 
 	// Session Stuff Logging >
-	if (ArgUtil.areEqual(sessionDoc.getAssignedToAgent(), agentSession.getAgentCode())) {
+	// if (ArgUtil.areEqual(sessionDoc.getAssignedToAgent(),
+	// agentSession.getAgentCode()) || agentSession.isAdmin()) {
+	if (ArgUtil.is(sessionDoc)) {
 	    outboxMessage.setAction(CHAT_SESSION_ACTIONS.ADD_STICKY_NOTE);
 	    ChatMessageDTO messageDto = agentService.sendMessage(sessionDoc, outboxMessage);
 	    // Evaluate if required
