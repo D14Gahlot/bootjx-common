844b0fe4b8329a0dcc03830877691e85975ede59
lalit.tanwar <lalit.tanwar07@gmail.com>
2021-12-02 14:07:58 +0530

    data api

--- server-xms/src/main/java/com/boot/jx/xms/api/DigitalApiV1.java
+++ server-xms/src/main/java/com/boot/jx/xms/api/DigitalApiV1.java
@@ -1,6 +1,7 @@
 package com.boot.jx.xms.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,8 +13,10 @@ import com.boot.jx.xms.dto.DigitalEventDto;
 import com.boot.jx.xms.dto.DigitalObjectDto;
 import com.boot.jx.xms.service.ApiService;
 
-//@Api(tags = "Digital Analytics", description = "Analytics APIS")
-//@Controller
+import io.swagger.annotations.Api;
+
+@Api(tags = "Digital Analytics", description = "Analytics APIS")
+@Controller
 public class DigitalApiV1 {
 
     @Autowired
