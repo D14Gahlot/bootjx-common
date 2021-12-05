045c11fe288a8e38d23f694028c57d4d3018ec47
lalit.tanwar <lalit.tanwar07@gmail.com>
2021-12-06 02:03:27 +0530

    hsm windoe

--- lib-common/lib-common-config/src/main/java/com/boot/jx/common/config/ConfigConstants.java
+++ lib-common/lib-common-config/src/main/java/com/boot/jx/common/config/ConfigConstants.java
@@ -54,10 +54,10 @@ public class ConfigConstants {
 		.defaultValue(PMConstants.ASSIGNMENT_RULE.ROUND_ROBIN)),
 
 	POSTMAN_AGENT_CHAT_AUTOREPLY_TALK2AGENT(new ConfigMeta("Message to customer while chat is transferred to agent",
-		"postman.agent.chat.autoreply.talk2agent").options("getx:/api/tmpl/pushtemplate")),
+		"postman.agent.chat.autoreply.talk2agent").options("getx:/api/tmpl/hsm")),
 
 	POSTMAN_AGENT_CHAT_AUTOREPLY_RESOLVED(new ConfigMeta("Message to customer when chat is resolevd by agent",
-		"postman.agent.chat.autoreply.resolved").options("getx:/api/tmpl/pushtemplate")),
+		"postman.agent.chat.autoreply.resolved").options("getx:/api/tmpl/hsm")),
 
 	POSTMAN_AGENT_CHAT_STICKYSESSION(
 		new ConfigMeta("Sticky Session", "postman.agent.chat.stickysession")
