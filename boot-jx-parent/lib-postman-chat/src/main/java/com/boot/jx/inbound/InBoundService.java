2188d5d79ffbebc67caf3565e78af5effc1f765a
lalit.tanwar <lalit.tanwar07@gmail.com>
2022-01-17 22:26:23 +0530

    boot

--- boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/inbound/InBoundService.java
+++ boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/inbound/InBoundService.java
@@ -179,7 +179,7 @@ public class InBoundService {
 	    if (ArgUtil.is(inBoundProcessor)) {
 		inBoundProcessor.process(inboxMessageOriginal);
 	    }
-	    if (chatClientConfig.isLocalDummyBotEnabled() && false) {
+	    if (chatClientConfig.isLocalDummyBotEnabled()) {
 		botEngine.invokeMethodsAsync(inboxMessageOriginal);
 	    } else if (ArgUtil.is(inBoundHandler)) {
 		inBoundHandler.handle(inboxMessageOriginal);
