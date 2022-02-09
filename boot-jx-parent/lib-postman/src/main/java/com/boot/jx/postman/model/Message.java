36e53ae729bc897657e59d9f2e41b99dc905d38e
lalit.tanwar <lalit.tanwar07@gmail.com>
2022-02-09 20:58:38 +0530

    lang setup

--- boot-jx-parent/lib-postman/src/main/java/com/boot/jx/postman/model/Message.java
+++ boot-jx-parent/lib-postman/src/main/java/com/boot/jx/postman/model/Message.java
@@ -311,7 +311,7 @@ public class Message<T extends Message<T>> implements Serializable, MessageOptio
     }
 
     @SuppressWarnings("unchecked")
-    public T lang(Language lang) {
+    public T lang(Object lang) {
 	this.hsm().setLang(ArgUtil.parseAsString(lang));
 	return (T) this;
     }
