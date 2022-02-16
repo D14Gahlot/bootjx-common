da1b47b1c9d67c97179d49919f0d68beab2ee18a
lalit.tanwar <lalit.tanwar07@gmail.com>
2022-02-16 15:12:06 +0530

    added admin inbound handler

---
+++ server-admin/src/main/java/com/boot/jx/admin/AdminInBoundHandler.java
@@ -0,0 +1,10 @@
+package com.boot.jx.admin;
+
+import org.springframework.stereotype.Component;
+
+import com.boot.jx.common.config.DefaultInBoundHandler;
+
+@Component
+public class AdminInBoundHandler extends DefaultInBoundHandler {
+    
+}
