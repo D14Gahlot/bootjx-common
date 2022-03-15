fb71274f826fc854bc91cc6f23f71c542d5c0b62
lalit.tanwar <lalit.tanwar07@gmail.com>
2022-03-16 00:28:15 +0530

    config

--- server-admin/src/main/java/com/boot/jx/admin/api/ConfigController.java
+++ server-admin/src/main/java/com/boot/jx/admin/api/ConfigController.java
@@ -80,8 +80,8 @@ public class ConfigController {
 
     @JsonView(PMEnvironment.PublicProperty.class)
     @ResponseBody
-    @RequestMapping(value = { "/api/config/clientapikey" }, method = { RequestMethod.DELETE })
-    public ApiResponse<ClientAppConfigDoc, Object> deleteClientApiKey(@RequestParam String id) {
+    @RequestMapping(value = { "/api/config/clientapikey/{id}" }, method = { RequestMethod.DELETE })
+    public ApiResponse<ClientAppConfigDoc, Object> deleteClientApiKey(@PathVariable String id) {
 	ClientAppConfigDoc clientApiKey = new ClientAppConfigDoc();
 	clientApiKey.setId(id);
 	return ApiResponse.buildResults(configManager.remove(clientApiKey));
