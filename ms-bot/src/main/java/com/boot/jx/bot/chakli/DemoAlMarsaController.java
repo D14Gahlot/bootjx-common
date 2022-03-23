1e211805af1e26682195071a8c855df87f8b5a5d
lalit.tanwar <lalit.tanwar07@gmail.com>
2022-03-23 09:23:12 +0530

    unused imports

--- ms-bot/src/main/java/com/boot/jx/bot/chakli/DemoAlMarsaController.java
+++ ms-bot/src/main/java/com/boot/jx/bot/chakli/DemoAlMarsaController.java
@@ -2,13 +2,10 @@ package com.boot.jx.bot.chakli;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
 
 import com.boot.jx.bot.BotController;
 import com.boot.jx.bot.ChatMapping;
 import com.boot.jx.bot.alex.AlexBotConstants;
-import com.boot.jx.postman.PMEnvironment;
 import com.boot.jx.postman.model.InboxMessage;
 import com.boot.jx.postman.model.OutboxMessage;
 import com.boot.utils.ArgUtil;
