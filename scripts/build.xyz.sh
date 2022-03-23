d26ac6e989c50a0f2810a99163bdaba8063e971a
lalit.tanwar <lalit.tanwar07@gmail.com>
2022-03-23 23:26:08 +0530

    added xyz

--- scripts/build.xyz.sh
+++ scripts/build.xyz.sh
@@ -5,15 +5,15 @@ echo "input  $input"
 
 if [[ $input == "-a" ]]
 then
-	git up && git co staging && git up && git merge master && git up && git pu && git co master && git merge staging
+	git up && git co xyz && git up && git merge master && git up && git pu && git co master && git merge xyz
 	for service in 'account' 'admin' 'agent' 'bot' 'postman' 'xms'
 	do
-		git co staging && git up && git pu && git co "build-xyz-${service}" && git up && git pu && git merge staging && git pu && git co staging
+		git co xyz && git up && git pu && git co "build-xyz-${service}" && git up && git pu && git merge xyz && git pu && git co xyz
 	done
 elif [[ $input == "-s" ]]
 then
-	git up && git co staging && git up && git merge master && git up && git pu && git co master && git merge staging
+	git up && git co xyz && git up && git merge master && git up && git pu && git co master && git merge xyz
 else	
-	git co staging && git up && git pu && git co "build-xyz-${input}" && git up && git pu && git merge staging && git pu
-	git co staging && git merge "build-xyz-${input}" && git pu
+	git co xyz && git up && git pu && git co "build-xyz-${input}" && git up && git pu && git merge xyz && git pu
+	git co xyz && git merge "build-xyz-${input}" && git pu
 fi	
