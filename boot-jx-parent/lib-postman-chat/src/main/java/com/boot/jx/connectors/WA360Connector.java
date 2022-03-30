02044b8435c0a7cb63a21d66546c7adb1ec8283b
lalit.tanwar <lalit.tanwar07@gmail.com>
2022-03-30 20:10:57 +0530

    id foixes

--- boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/connectors/WA360Connector.java
+++ boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/connectors/WA360Connector.java
@@ -262,7 +262,7 @@ public class WA360Connector extends AbstractConnector<WA360ConfigDetails, WA360P
 					Map<String, Object> conversation = statusModel.keyEntry("conversation").asMap();
 					if (ArgUtil.is(conversation)) {
 						String id = ArgUtil.parseAsString(conversation.get("id"));
-						WABAConversationQuery query = new WABAConversationQuery(new WABAConversation());
+						WABAConversationQuery query = new WABAConversationQuery(id);
 						query.setContact(reprt.contact());
 						query.setConversation(conversation);
 						query.setPricing(statusModel.keyEntry("pricing").asMap());
