1cb88f4d2223bd13f4d34f70af572fafe1fba551
lalit.tanwar <lalit.tanwar07@gmail.com>
2022-04-02 20:43:55 +0530

    File Updoad fixed

--- boot-jx-parent/common-lib/src/main/java/com/boot/jx/dict/FileFormat.java
+++ boot-jx-parent/common-lib/src/main/java/com/boot/jx/dict/FileFormat.java
@@ -3,67 +3,69 @@ package com.boot.jx.dict;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.boot.utils.ArgUtil;
 import com.boot.utils.EnumType;
 
 public enum FileFormat implements EnumType {
-    PDF("application/pdf"), CSV("text/csv"),
+	PDF("application/pdf"), CSV("text/csv"),
 
-    PNG("image/png", FileType.IMAGE), JPEG("image/jpeg", FileType.IMAGE), JPG("image/jpg", FileType.IMAGE),
-    BMP("image/bmp", FileType.IMAGE), GIF("image/gif", FileType.IMAGE), TIFF("image/tiff", FileType.IMAGE),
-    TIF("image/tif", FileType.IMAGE),
+	PNG("image/png", FileType.IMAGE), JPEG("image/jpeg", FileType.IMAGE), JPG("image/jpg", FileType.IMAGE),
+	BMP("image/bmp", FileType.IMAGE), GIF("image/gif", FileType.IMAGE), TIFF("image/tiff", FileType.IMAGE),
+	TIF("image/tif", FileType.IMAGE),
 
-    WEBP("image/webp", FileType.IMAGE),
+	WEBP("image/webp", FileType.IMAGE),
 
-    MP3("audio/mp3", FileType.AUDIO), aac("audio/aac", FileType.AUDIO), AMR("audio/amr", FileType.AUDIO),
+	MP3("audio/mp3", FileType.AUDIO), aac("audio/aac", FileType.AUDIO), AMR("audio/amr", FileType.AUDIO),
 
-    OGG("audio/ogg", FileType.AUDIO), OGG_PLUS("audio/ogg; codecs=opus", FileType.AUDIO),
+	OGG("audio/ogg", FileType.AUDIO), OGG_PLUS("audio/ogg; codecs=opus", FileType.AUDIO),
 
-    AUDIO_MP4("audio/mp4", FileType.AUDIO), AUDIO_MPEG("audio/mpeg", FileType.AUDIO),
+	AUDIO_MP4("audio/mp4", FileType.AUDIO), AUDIO_MPEG("audio/mpeg", FileType.AUDIO),
 
-    MP4("video/mp4", FileType.VIDEO), VIDEO_3GPP("video/3gpp", FileType.VIDEO),
+	MP4("video/mp4", FileType.VIDEO), VIDEO_3GPP("video/3gpp", FileType.VIDEO),
 
-    JSON("application/json"), HTML("text/html"), TEXT("text/plain", FileType.TEXT), UNKNOWN("application/octet-stream");
+	JSON("application/json"), HTML("text/html"), TEXT("text/plain", FileType.TEXT), UNKNOWN("application/octet-stream");
 
-    private static final Map<String, FileFormat> TYPEMAP = new HashMap<String, FileFormat>();
+	private static final Map<String, FileFormat> TYPEMAP = new HashMap<String, FileFormat>();
 
-    static {
-	for (FileFormat fileFormat : FileFormat.values()) {
-	    TYPEMAP.put(fileFormat.getContentType(), fileFormat);
+	static {
+		for (FileFormat fileFormat : FileFormat.values()) {
+			TYPEMAP.put(fileFormat.getContentType(), fileFormat);
+			// application/vnd.openxmlformats-officedocument.wordprocessingml.document
+		}
 	}
-    }
 
-    String contentType;
-    FileType fileType;
+	String contentType;
+	FileType fileType;
 
-    public String getContentType() {
-	return contentType;
-    }
+	public String getContentType() {
+		return contentType;
+	}
 
-    FileFormat(String contentType, FileType formatType) {
-	this.contentType = contentType;
-	this.fileType = formatType;
+	FileFormat(String contentType, FileType formatType) {
+		this.contentType = contentType;
+		this.fileType = formatType;
 
-    }
+	}
 
-    FileFormat(String contentType) {
-	this(contentType, FileType.DOCUMENT);
-    }
+	FileFormat(String contentType) {
+		this(contentType, FileType.DOCUMENT);
+	}
 
-    public static FileFormat from(String contentType) {
-	return TYPEMAP.get(contentType);
-    }
+	public static FileFormat from(String contentType) {
+		return TYPEMAP.getOrDefault(contentType, UNKNOWN);
+	}
 
-    public static FileFormat from(String contentType, FileFormat defaultValue) {
-	return TYPEMAP.getOrDefault(contentType, defaultValue);
-    }
+	public static FileFormat from(String contentType, FileFormat defaultValue) {
+		return TYPEMAP.getOrDefault(contentType, ArgUtil.nonEmpty(defaultValue, UNKNOWN));
+	}
 
-    /**
-     * IMAGE,TEXT, DOCUMENT,VIDEO
-     * 
-     * @return
-     */
-    public FileType getFileType() {
-	return fileType;
-    }
+	/**
+	 * IMAGE,TEXT, DOCUMENT,VIDEO
+	 * 
+	 * @return
+	 */
+	public FileType getFileType() {
+		return fileType;
+	}
 
 }
--- boot-jx-parent/lib-aws/src/main/java/com/boot/jx/aws/AWSFileStore.java
+++ boot-jx-parent/lib-aws/src/main/java/com/boot/jx/aws/AWSFileStore.java
@@ -21,104 +21,104 @@ import com.boot.utils.ArgUtil;
 @Component
 public class AWSFileStore {
 
-    @Autowired
-    private AWSConfig awsConfig;
+	@Autowired
+	private AWSConfig awsConfig;
 
-    public void upload(AmazonS3 amazonS3, String path, String fileName, ObjectMetadata objectMetadata,
-	    InputStream inputStream) {
-	try {
-	    amazonS3.putObject(path, fileName, inputStream, objectMetadata);
-	} catch (AmazonServiceException e) {
-	    throw new IllegalStateException("Failed to upload the file", e);
-	}
-    }
-
-    private CommonFile createFile(AmazonS3 amazonS3, String bucketName, String pathFolder, String fileName,
-	    CommonFile srcFile) {
-
-	if (!ArgUtil.is(srcFile.getContentType())) {
-	    throw new IllegalStateException("File uploaded is not an accepted format");
+	public void upload(AmazonS3 amazonS3, String path, String fileName, ObjectMetadata objectMetadata,
+			InputStream inputStream) {
+		try {
+			amazonS3.putObject(path, fileName, inputStream, objectMetadata);
+		} catch (AmazonServiceException e) {
+			throw new IllegalStateException("Failed to upload the file", e);
+		}
 	}
 
-	// Save Image in S3 and then save Todo in the database
-	String fileNameNow = String.format("%s", fileName);
+	private CommonFile createFile(AmazonS3 amazonS3, String bucketName, String pathFolder, String fileName,
+			CommonFile srcFile) {
 
-	return new CommonFile()
-		.url(String.format("http://%s.s3.amazonaws.com/%s/%s", bucketName, pathFolder, fileNameNow))
-		.path(pathFolder).name(fileNameNow).format(srcFile.getFileFormat());
-    }
+		if (!ArgUtil.is(srcFile.getContentType())) {
+			throw new IllegalStateException("File uploaded is not an accepted format");
+		}
 
-    private CommonFile commitFile(AmazonS3 amazonS3, String bucketName, CommonFile dstFile, MultipartFile file) {
-	// check if the file is empty
-	if (!ArgUtil.is(file) || file.isEmpty()) {
-	    throw new IllegalStateException("Cannot upload empty file");
+		// Save Image in S3 and then save Todo in the database
+		String fileNameNow = String.format("%s", fileName);
+
+		return new CommonFile()
+				.url(String.format("http://%s.s3.amazonaws.com/%s/%s", bucketName, pathFolder, fileNameNow))
+				.path(pathFolder).name(fileNameNow).format(srcFile.getFileFormat());
 	}
 
-	FileFormat fileFormat = FileFormat.from(file.getContentType());
-	dstFile.setFileFormat(fileFormat);
+	private CommonFile commitFile(AmazonS3 amazonS3, String bucketName, CommonFile dstFile, MultipartFile file) {
+		// check if the file is empty
+		if (!ArgUtil.is(file) || file.isEmpty()) {
+			throw new IllegalStateException("Cannot upload empty file");
+		}
 
-	// Check if the file is an image
-	if (!ArgUtil.is(dstFile.getFileFormat())) {
-	    throw new IllegalStateException("File uploaded is not an accepted format");
+		FileFormat fileFormat = FileFormat.from(file.getContentType());
+		dstFile.setFileFormat(fileFormat);
+
+		// Check if the file is an image
+		if (!ArgUtil.is(dstFile.getFileFormat())) {
+			throw new IllegalStateException("File uploaded is not an accepted format");
+		}
+
+		// get file metadata
+		ObjectMetadata objectMetadata = new ObjectMetadata();
+		objectMetadata.setContentType(file.getContentType());
+		objectMetadata.setContentLength(file.getSize());
+
+		// Save Image in S3 and then save Todo in the database
+		String bucketPath = String.format("%s/%s", bucketName, dstFile.getPath());
+
+		try {
+			upload(amazonS3, bucketPath, dstFile.getName(), objectMetadata, file.getInputStream());
+		} catch (IOException e) {
+			throw new IllegalStateException("Failed to upload file", e);
+		}
+
+		return dstFile;
 	}
 
-	// get file metadata
-	ObjectMetadata objectMetadata = new ObjectMetadata();
-	objectMetadata.setContentType(file.getContentType());
-	objectMetadata.setContentLength(file.getSize());
-
-	// Save Image in S3 and then save Todo in the database
-	String bucketPath = String.format("%s/%s", bucketName, dstFile.getPath());
-
-	try {
-	    upload(amazonS3, bucketPath, dstFile.getName(), objectMetadata, file.getInputStream());
-	} catch (IOException e) {
-	    throw new IllegalStateException("Failed to upload file", e);
+	private CommonFile upload(AmazonS3 amazonS3, String bucketName, String pathFolder, String fileName,
+			MultipartFile file) {
+		CommonFile dstFile = createFile(amazonS3, bucketName, pathFolder, fileName,
+				new CommonFile().contentType(file.getContentType()));
+		return commitFile(amazonS3, bucketName, dstFile, file);
 	}
 
-	return dstFile;
-    }
-
-    private CommonFile upload(AmazonS3 amazonS3, String bucketName, String pathFolder, String fileName,
-	    MultipartFile file) {
-	CommonFile dstFile = createFile(amazonS3, bucketName, pathFolder, fileName,
-		new CommonFile().contentType(file.getContentType()));
-	return commitFile(amazonS3, bucketName, dstFile, file);
-    }
-
-    public CommonFile upload1(MultipartFile file, String pathFolder, String fileName) {
-	return upload(awsConfig.getS3B1(), awsConfig.getS3B1Name(), pathFolder, fileName, file);
-    }
-
-    public CommonFile upload2(MultipartFile file, String pathFolder, String fileName) {
-	return upload(awsConfig.getS3B2(), awsConfig.getS3B2Name(), pathFolder, fileName, file);
-    }
-
-    public CommonFile createFile2(CommonFile srcFile, String pathFolder, String fileName) {
-	return createFile(awsConfig.getS3B2(), awsConfig.getS3B2Name(), pathFolder, fileName, srcFile);
-    }
-
-    @Async
-    public CommonFile commitFile2(MultipartFile srcFile, CommonFile dstFile) {
-	return commitFile(awsConfig.getS3B2(), awsConfig.getS3B2Name(), dstFile, srcFile);
-    }
-
-    @Async
-    public CommonFile commitFile2(CommonFile srcFile, CommonFile dstFile) {
-	MultipartFile srcMultipartFile = new CommonFile().url(srcFile.getUrl()).headers(srcFile.getHeaders())
-		.format(dstFile.getFileFormat()).name(dstFile.getName()).toMultipartFile();
-	return commitFile(awsConfig.getS3B2(), awsConfig.getS3B2Name(), dstFile, srcMultipartFile);
-    }
-
-    public byte[] download1(String pathFolder, String key) {
-	try {
-	    String path = String.format("%s/%s", awsConfig.getS3B1Name(), pathFolder);
-	    S3Object object = awsConfig.getS3B1().getObject(path, key);
-	    S3ObjectInputStream objectContent = object.getObjectContent();
-	    return IOUtils.toByteArray(objectContent);
-	} catch (AmazonServiceException | IOException e) {
-	    throw new IllegalStateException("Failed to download the file", e);
+	public CommonFile upload1(MultipartFile file, String pathFolder, String fileName) {
+		return upload(awsConfig.getS3B1(), awsConfig.getS3B1Name(), pathFolder, fileName, file);
 	}
-    }
+
+	public CommonFile upload2(MultipartFile file, String pathFolder, String fileName) {
+		return upload(awsConfig.getS3B2(), awsConfig.getS3B2Name(), pathFolder, fileName, file);
+	}
+
+	public CommonFile createFile2(CommonFile srcFile, String pathFolder, String fileName) {
+		return createFile(awsConfig.getS3B2(), awsConfig.getS3B2Name(), pathFolder, fileName, srcFile);
+	}
+
+	@Async
+	public CommonFile commitFile2(MultipartFile srcFile, CommonFile dstFile) {
+		return commitFile(awsConfig.getS3B2(), awsConfig.getS3B2Name(), dstFile, srcFile);
+	}
+
+	@Async
+	public CommonFile commitFile2(CommonFile srcFile, CommonFile dstFile) {
+		MultipartFile srcMultipartFile = new CommonFile().url(srcFile.getUrl()).headers(srcFile.getHeaders())
+				.format(dstFile.getFileFormat()).name(dstFile.getName()).toMultipartFile();
+		return commitFile(awsConfig.getS3B2(), awsConfig.getS3B2Name(), dstFile, srcMultipartFile);
+	}
+
+	public byte[] download1(String pathFolder, String key) {
+		try {
+			String path = String.format("%s/%s", awsConfig.getS3B1Name(), pathFolder);
+			S3Object object = awsConfig.getS3B1().getObject(path, key);
+			S3ObjectInputStream objectContent = object.getObjectContent();
+			return IOUtils.toByteArray(objectContent);
+		} catch (AmazonServiceException | IOException e) {
+			throw new IllegalStateException("Failed to download the file", e);
+		}
+	}
 
 }
