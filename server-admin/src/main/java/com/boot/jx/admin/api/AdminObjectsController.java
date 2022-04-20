a0b3b106d2094d853fa8cffef3bb7ae7a4ae2427
lalit.tanwar <lalit.tanwar07@gmail.com>
2022-04-20 22:00:38 +0530

    added order sorting
    Signed-off-by: lalit.tanwar <lalit.tanwar07@gmail.com>

--- server-admin/src/main/java/com/boot/jx/admin/api/AdminObjectsController.java
+++ server-admin/src/main/java/com/boot/jx/admin/api/AdminObjectsController.java
@@ -1,6 +1,7 @@
 package com.boot.jx.admin.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,7 @@ import com.boot.jx.mongo.CommonMongoQueryBuilder;
 import com.boot.jx.postman.PMEnvironment;
 import com.boot.jx.postman.doc.MessageDoc.MessageDocLogs;
 import com.boot.jx.postman.store.MessageStore;
+import com.boot.utils.ArgUtil;
 import com.fasterxml.jackson.annotation.JsonView;
 
 @RestController
@@ -24,9 +26,16 @@ public class AdminObjectsController {
 	@JsonView(PMEnvironment.PublicProperty.class)
 	public ApiResponse<MessageDocLogs, Object> getLogs(@RequestParam(required = false) String id,
 			@RequestParam(required = false, defaultValue = "0") int pageNo,
-			@RequestParam(required = false, defaultValue = "25") int pageSize) {
+			@RequestParam(required = false, defaultValue = "25") int pageSize,
+			@RequestParam(required = false) String sortBy,
+			@RequestParam(required = false, defaultValue = "asc") String sortDir) {
 		CommonMongoQBimpl<MessageDocLogs> q = CommonMongoQueryBuilder.collection(MessageDocLogs.class).page(pageNo,
 				pageSize);
+
+		if (ArgUtil.is(sortBy)) {
+			q.sortBy(sortBy, Direction.fromString(sortDir));
+		}
+
 		return ApiResponse.buildResults(messageStore.find(q));
 	}
 }
