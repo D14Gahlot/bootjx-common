67c72c6a5a1483f86b9da1dbf2540477f3e33ed4
lalit.tanwar <lalit.tanwar07@gmail.com>
2022-05-19 12:59:12 +0530

    admin build

--- boot-jx-parent/lib-postman-store/src/main/java/com/boot/jx/postman/manager/StarterDocKit.java
+++ boot-jx-parent/lib-postman-store/src/main/java/com/boot/jx/postman/manager/StarterDocKit.java
@@ -66,6 +66,45 @@ public class StarterDocKit {
 	@PostConstruct
 	public void init() {
 
+		try {
+			createDefaultTemplats();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		ClientAppConfigDoc agentApp = new ClientAppConfigDoc();
+		agentApp.setId(PMConstants.DEFAULT.AGENT_QUEUE_CODE);
+		agentApp.setKeyName("Agent Desk");
+		agentApp.setQueue(PMConstants.DEFAULT.AGENT_QUEUE_CODE);
+		agentApp.setAppType(APP_TYPE.AGENT.name());
+		agentApp.setKey(PostManUtil.UNIQUE_API_KEY());
+		agentApp.setKeyVersion("v3");
+		agentApp.setShared(true);
+		createClientApp(agentApp);
+
+		ClientAppConfigDoc botApp = new ClientAppConfigDoc();
+		botApp.setId(PMConstants.DEFAULT.BOT_QUEUE_CODE);
+		botApp.setKeyName("Basic Bot");
+		botApp.setQueue(PMConstants.DEFAULT.BOT_QUEUE_CODE);
+		botApp.setAppType(APP_TYPE.BOT.name());
+		botApp.setKey(PostManUtil.UNIQUE_API_KEY());
+		botApp.setKeyVersion("v3");
+		botApp.setShared(true);
+		createClientApp(botApp);
+
+		ClientAppConfigDoc adminApp = new ClientAppConfigDoc();
+		adminApp.setId(PMConstants.DEFAULT.ADMIN_QUEUE_CODE);
+		adminApp.setKeyName("Admin App");
+		adminApp.setQueue(PMConstants.DEFAULT.ADMIN_QUEUE_CODE);
+		adminApp.setAppType(APP_TYPE.DEFAULT.name());
+		adminApp.setKey(PostManUtil.UNIQUE_API_KEY());
+		adminApp.setKeyVersion("v4");
+		adminApp.setShared(true);
+		createClientApp(adminApp);
+	}
+
+	private void createDefaultTemplats() {
 		mongoTemplate.save(createTemplateReply("GIRL_AND_BIKE", "Girl and bike", "Gallery1", "See this Nice Pic",
 				"https://res.cloudinary.com/www-mehery-com/image/upload/v1611688334/samples/bike.jpg"));
 
@@ -100,36 +139,6 @@ public class StarterDocKit {
 		mongoTemplate.save(createQuickReply("3", "Very Good Evening", "greeting-evening"));
 		mongoTemplate.save(createQuickReply("4", "Nice talking too.", "conversation-complete"));
 		mongoTemplate.save(createQuickReply("5", "You're welcome.", "conversation-complete"));
-
-		ClientAppConfigDoc agentApp = new ClientAppConfigDoc();
-		agentApp.setId(PMConstants.DEFAULT.AGENT_QUEUE_CODE);
-		agentApp.setKeyName("Agent Desk");
-		agentApp.setQueue(PMConstants.DEFAULT.AGENT_QUEUE_CODE);
-		agentApp.setAppType(APP_TYPE.AGENT.name());
-		agentApp.setKey(PostManUtil.UNIQUE_API_KEY());
-		agentApp.setKeyVersion("v3");
-		agentApp.setShared(true);
-		createClientApp(agentApp);
-
-		ClientAppConfigDoc botApp = new ClientAppConfigDoc();
-		botApp.setId(PMConstants.DEFAULT.BOT_QUEUE_CODE);
-		botApp.setKeyName("Basic Bot");
-		botApp.setQueue(PMConstants.DEFAULT.BOT_QUEUE_CODE);
-		botApp.setAppType(APP_TYPE.BOT.name());
-		botApp.setKey(PostManUtil.UNIQUE_API_KEY());
-		botApp.setKeyVersion("v3");
-		botApp.setShared(true);
-		createClientApp(botApp);
-
-		ClientAppConfigDoc adminApp = new ClientAppConfigDoc();
-		adminApp.setId(PMConstants.DEFAULT.ADMIN_QUEUE_CODE);
-		adminApp.setKeyName("Admin App");
-		adminApp.setQueue(PMConstants.DEFAULT.ADMIN_QUEUE_CODE);
-		adminApp.setAppType(APP_TYPE.DEFAULT.name());
-		adminApp.setKey(PostManUtil.UNIQUE_API_KEY());
-		adminApp.setKeyVersion("v4");
-		adminApp.setShared(true);
-		createClientApp(adminApp);
 	}
 
 	public void domain() {
