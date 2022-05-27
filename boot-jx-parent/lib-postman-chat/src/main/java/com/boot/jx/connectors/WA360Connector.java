fe7bda5a93505267a9a5b827bafb7e6406d3f049
lalit.tanwar <lalit.tanwar07@gmail.com>
2022-05-27 23:35:17 +0530

    message location

--- boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/connectors/WA360Connector.java
+++ boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/connectors/WA360Connector.java
@@ -170,11 +170,11 @@ public class WA360Connector extends AbstractConnector<WA360ConfigDetails, WA360P
 		} else if ("location".equals(messageType)) {
 			inboxMessage.setFormatType(MESSAGE_FORMAT_TYPE.LOCATION);
 			PBLocation pbLocation = new PBLocation();
-			pbLocation.setAddress(map.pathEntry("/location/first_name").asString());
-			pbLocation.setLatitude(map.pathEntry("/location/latitude").asString());
-			pbLocation.setLongitude(map.pathEntry("/location/longitude").asString());
-			pbLocation.setName(map.pathEntry("/location/name").asString());
-			pbLocation.setUrl(map.pathEntry("/location/url").asString());
+			pbLocation.setName(map.pathEntry("location/name").asString());
+			pbLocation.setAddress(map.pathEntry("location/address").asString());
+			pbLocation.setLatitude(map.pathEntry("location/latitude").asString());
+			pbLocation.setLongitude(map.pathEntry("location/longitude").asString());
+			pbLocation.setUrl(map.pathEntry("location/url").asString());
 			inboxMessage.vccards().add(new PBVCard().locations(pbLocation));
 		}
 
