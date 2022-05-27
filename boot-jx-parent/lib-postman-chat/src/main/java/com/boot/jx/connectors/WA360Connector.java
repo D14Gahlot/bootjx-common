7b949261953c8d1fd5f035122f45dcf23701e495
lalit.tanwar <lalit.tanwar07@gmail.com>
2022-05-28 00:39:09 +0530

    latitude

--- boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/connectors/WA360Connector.java
+++ boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/connectors/WA360Connector.java
@@ -172,7 +172,7 @@ public class WA360Connector extends AbstractConnector<WA360ConfigDetails, WA360P
 			PBLocation pbLocation = new PBLocation();
 			pbLocation.setName(map.pathEntry("messages/[0]/location/name").asString());
 			pbLocation.setAddress(map.pathEntry("messages/[0]/location/address").asString());
-			pbLocation.setLatitude(map.pathEntry("lmessages/[0]/ocation/latitude").asString());
+			pbLocation.setLatitude(map.pathEntry("messages/[0]/location/latitude").asString());
 			pbLocation.setLongitude(map.pathEntry("messages/[0]/location/longitude").asString());
 			pbLocation.setUrl(map.pathEntry("messages/[0]/location/url").asString());
 			inboxMessage.vccards().add(new PBVCard().locations(pbLocation));
