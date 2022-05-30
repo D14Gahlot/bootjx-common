6456af8bbea5555f87c31f6d8624a1c89c0c2595
lalit.tanwar <lalit.tanwar07@gmail.com>
2022-05-31 01:30:58 +0530

    added all

--- scripts/build.qa.all.sh
+++ scripts/build.qa.all.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-bash scripts/sync.master_production.sh
+#bash scripts/sync.master_production.sh
 bash scripts/build.qa.sh account
 bash scripts/build.qa.sh admin
 bash scripts/build.qa.sh bot
