77ef862e72a5d095347fe2d720f3580aaf83fc9a
rabiluddin <rabiluddin@gmail.com>
2022-06-07 18:27:10 +0300

    null check

--- server-account/src/main/java/com/boot/jx/account/manager/AccountDashBoardManager.java
+++ server-account/src/main/java/com/boot/jx/account/manager/AccountDashBoardManager.java
@@ -247,12 +247,14 @@ public class AccountDashBoardManager {
 					dto.setDomain(tnt);
 					String id = getSummaryId(dto);
 					dto.setId(id);
-					
 					//LOGGER.info("datewaise data :" + JsonUtil.toJson(dto));
+					if(ArgUtil.is(dto.getId())) {
 					lstSummDto.add(dto);
+					}
 				}
 
 			}
+		
 			Map<Object, Long> summaryMap = new HashMap<>();
 			Map<String, Map<String, Long>> datwWiseCount = lstSummDto.stream().collect(Collectors.groupingBy(SummaryDocDto::getId, Collectors.groupingBy(SummaryDocDto::getType, Collectors.counting())));
 		
