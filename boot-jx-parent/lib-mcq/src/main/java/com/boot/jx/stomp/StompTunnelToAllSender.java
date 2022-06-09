e7b8a440c51f95b2e5e4cc896890685495e323e1
lalit.tanwar <lalit.tanwar07@gmail.com>
2022-06-09 08:25:13 +0530

    stomp all

--- boot-jx-parent/lib-mcq/src/main/java/com/boot/jx/stomp/StompTunnelToAllSender.java
+++ boot-jx-parent/lib-mcq/src/main/java/com/boot/jx/stomp/StompTunnelToAllSender.java
@@ -1,10 +1,7 @@
 package com.boot.jx.stomp;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.messaging.core.MessageSendingOperations;
 
 import com.boot.jx.tunnel.ITunnelSubscriber;
 import com.boot.jx.tunnel.TunnelEventMapping;
@@ -15,19 +12,15 @@ import com.boot.jx.tunnel.TunnelEventXchange;
 @ConditionalOnProperty("app.stomp")
 public class StompTunnelToAllSender implements ITunnelSubscriber<StompTunnelEvent> {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(StompTunnelToAllSender.class);
-
 	public static final String STOMP_TO_ALL = "STOMP_TO_ALL";
 
 	@Autowired(required = false)
-	private MessageSendingOperations<String> messagingTemplate;
-
-	@Autowired
 	StompTunnelToLocally stompTunnelToLocally;
 
 	@Override
 	public void onMessage(String channel, StompTunnelEvent msg) {
-		stompTunnelToLocally.sendToAll(msg);
+		if (stompTunnelToLocally != null)
+			stompTunnelToLocally.sendToAll(msg);
 	}
 
 }
