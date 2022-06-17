5bca33ce803bdc41287c362c6365f6ea4bef86fb
lalit.tanwar <lalit.tanwar07@gmail.com>
2022-06-17 12:02:19 +0530

    webhook

--- lib-common/lib-common-config/src/main/java/com/boot/jx/common/config/DefaultChatBoundHandler.java
+++ lib-common/lib-common-config/src/main/java/com/boot/jx/common/config/DefaultChatBoundHandler.java
@@ -123,13 +123,11 @@ public abstract class DefaultChatBoundHandler implements InBoundHandler {
 						// production
 						inboxMessage.setOriginalMessage(null);
 						chatClient.forward(defaultClient.getForward() + PATH.INBOUND_FRWRD, inboxMessage);
+					} else if (APP_TYPE.APP_SCRIPT.equals(appType)) {
+						forward2Webhook(inboxMessage, pmCommonConfig.getScriptusUrl() + PATH.APP_SCRIPT_FRWRD,
+								defaultClient.getId());
 					} else if (ArgUtil.is(defaultClient.getWebhook())) {
-						if (APP_TYPE.APP_SCRIPT.equals(appType)) {
-							forward2Webhook(inboxMessage, pmCommonConfig.getScriptusUrl() + PATH.APP_SCRIPT_FRWRD,
-									defaultClient.getId());
-						} else {
-							forward2Webhook(inboxMessage, defaultClient.getWebhook(), defaultClient.getId());
-						}
+						forward2Webhook(inboxMessage, defaultClient.getWebhook(), defaultClient.getId());
 					} else {
 						ApiResponseUtil.throwException("Forward URL missing");
 					}
