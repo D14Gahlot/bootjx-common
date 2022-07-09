3196beaa42d785007a2e76277ceb7ab1da3f49a4
lalit.tanwar <lalit.tanwar07@gmail.com>
2022-07-09 23:17:04 +0530

    email phone

--- boot-jx-parent/lib-postman/src/main/java/com/boot/jx/postman/dto/ContactDTO.java
+++ boot-jx-parent/lib-postman/src/main/java/com/boot/jx/postman/dto/ContactDTO.java
@@ -10,214 +10,232 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ContactDTO implements java.io.Serializable, Contactable {
 
-    private static final long serialVersionUID = 8977954934029643371L;
+	private static final long serialVersionUID = 8977954934029643371L;
 
-    @ApiMockModelProperty(example = "wa919876543210", required = false)
-    @JsonProperty("contactId")
-    private String contactId;
+	@ApiMockModelProperty(example = "wa919876543210", required = false)
+	@JsonProperty("contactId")
+	private String contactId;
 
-    @JsonProperty("contactType")
-    private String contactType;
+	@JsonProperty("contactType")
+	private String contactType;
 
-    private String channelType;
+	private String channelType;
 
-    private String lane;
+	private String lane;
 
-    private String csid;
+	private String csid;
 
-    @ApiMockModelProperty(example = "John Doe", required = false)
-    private String name;
+	@ApiMockModelProperty(example = "John Doe", required = false)
+	private String name;
 
-    @ApiMockModelProperty(example = "John.Doe@company.co", required = false)
-    private String email;
+	@ApiMockModelProperty(example = "John.Doe@company.co", required = false)
+	private String email;
+	private Boolean emailVerified;
 
-    @ApiMockModelProperty(example = "919876543210", required = false)
-    private String phone;
+	@ApiMockModelProperty(example = "919876543210", required = false)
+	private String phone;
+	private Boolean phoneVerified;
 
-    private String profilePic;
+	private String profilePic;
 
-    private List<String> labelId;
-    private ChatUserProfileDTO profile;
+	private List<String> labelId;
+	private ChatUserProfileDTO profile;
 
-    private long firstInBoundStamp;
-    private long firstOutBoundStamp;
-    private long lastInBoundStamp;
-    private long lastOutBoundStamp;
-    private long lastPushStamp;
-    private long lastReplyStamp;
-    private long lastOptInStamp;
+	private long firstInBoundStamp;
+	private long firstOutBoundStamp;
+	private long lastInBoundStamp;
+	private long lastOutBoundStamp;
+	private long lastPushStamp;
+	private long lastReplyStamp;
+	private long lastOptInStamp;
 
-    private Long createdStamp;
-    private String createdBy;
+	private Long createdStamp;
+	private String createdBy;
 
-    private String sessionId;
+	private String sessionId;
 
-    public String getContactId() {
-	return contactId;
-    }
+	public String getContactId() {
+		return contactId;
+	}
 
-    public void setContactId(String contactId) {
-	this.contactId = contactId;
-    }
+	public void setContactId(String contactId) {
+		this.contactId = contactId;
+	}
 
-    public String getName() {
-	return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-	this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getEmail() {
-	return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setEmail(String email) {
-	this.email = email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public String getPhone() {
-	return phone;
-    }
+	public String getPhone() {
+		return phone;
+	}
 
-    public void setPhone(String phone) {
-	this.phone = phone;
-    }
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
-    public List<String> getLabelId() {
-	return labelId;
-    }
+	public List<String> getLabelId() {
+		return labelId;
+	}
 
-    public void setLabelId(List<String> labelId) {
-	this.labelId = labelId;
-    }
+	public void setLabelId(List<String> labelId) {
+		this.labelId = labelId;
+	}
 
-    public String getContactType() {
-	return contactType;
-    }
+	public String getContactType() {
+		return contactType;
+	}
 
-    public void setContactType(String contactType) {
-	this.contactType = contactType;
-    }
+	public void setContactType(String contactType) {
+		this.contactType = contactType;
+	}
 
-    public String getProfilePic() {
-	return profilePic;
-    }
+	public String getProfilePic() {
+		return profilePic;
+	}
 
-    public void setProfilePic(String profilePic) {
-	this.profilePic = profilePic;
-    }
+	public void setProfilePic(String profilePic) {
+		this.profilePic = profilePic;
+	}
 
-    public ChatUserProfileDTO getProfile() {
-	return profile;
-    }
+	public ChatUserProfileDTO getProfile() {
+		return profile;
+	}
 
-    public void setProfile(ChatUserProfileDTO profile) {
-	this.profile = profile;
-    }
+	public void setProfile(ChatUserProfileDTO profile) {
+		this.profile = profile;
+	}
 
-    public String getLane() {
-	return lane;
-    }
+	public String getLane() {
+		return lane;
+	}
 
-    public void setLane(String lane) {
-	this.lane = lane;
-    }
+	public void setLane(String lane) {
+		this.lane = lane;
+	}
 
-    public String getCsid() {
-	return csid;
-    }
+	public String getCsid() {
+		return csid;
+	}
 
-    public void setCsid(String csid) {
-	this.csid = csid;
-    }
+	public void setCsid(String csid) {
+		this.csid = csid;
+	}
 
-    public long getFirstInBoundStamp() {
-	return firstInBoundStamp;
-    }
+	public long getFirstInBoundStamp() {
+		return firstInBoundStamp;
+	}
 
-    public void setFirstInBoundStamp(long firstInBoundStamp) {
-	this.firstInBoundStamp = firstInBoundStamp;
-    }
+	public void setFirstInBoundStamp(long firstInBoundStamp) {
+		this.firstInBoundStamp = firstInBoundStamp;
+	}
 
-    public long getFirstOutBoundStamp() {
-	return firstOutBoundStamp;
-    }
+	public long getFirstOutBoundStamp() {
+		return firstOutBoundStamp;
+	}
 
-    public void setFirstOutBoundStamp(long firstOutBoundStamp) {
-	this.firstOutBoundStamp = firstOutBoundStamp;
-    }
+	public void setFirstOutBoundStamp(long firstOutBoundStamp) {
+		this.firstOutBoundStamp = firstOutBoundStamp;
+	}
 
-    public long getLastInBoundStamp() {
-	return lastInBoundStamp;
-    }
+	public long getLastInBoundStamp() {
+		return lastInBoundStamp;
+	}
 
-    public void setLastInBoundStamp(long lastInBoundStamp) {
-	this.lastInBoundStamp = lastInBoundStamp;
-    }
+	public void setLastInBoundStamp(long lastInBoundStamp) {
+		this.lastInBoundStamp = lastInBoundStamp;
+	}
 
-    public long getLastOutBoundStamp() {
-	return lastOutBoundStamp;
-    }
+	public long getLastOutBoundStamp() {
+		return lastOutBoundStamp;
+	}
 
-    public void setLastOutBoundStamp(long lastOutBoundStamp) {
-	this.lastOutBoundStamp = lastOutBoundStamp;
-    }
+	public void setLastOutBoundStamp(long lastOutBoundStamp) {
+		this.lastOutBoundStamp = lastOutBoundStamp;
+	}
 
-    public long getLastPushStamp() {
-	return lastPushStamp;
-    }
+	public long getLastPushStamp() {
+		return lastPushStamp;
+	}
 
-    public void setLastPushStamp(long lastPushStamp) {
-	this.lastPushStamp = lastPushStamp;
-    }
+	public void setLastPushStamp(long lastPushStamp) {
+		this.lastPushStamp = lastPushStamp;
+	}
 
-    public long getLastReplyStamp() {
-	return lastReplyStamp;
-    }
+	public long getLastReplyStamp() {
+		return lastReplyStamp;
+	}
 
-    public void setLastReplyStamp(long lastReplyStamp) {
-	this.lastReplyStamp = lastReplyStamp;
-    }
+	public void setLastReplyStamp(long lastReplyStamp) {
+		this.lastReplyStamp = lastReplyStamp;
+	}
 
-    public long getLastOptInStamp() {
-	return lastOptInStamp;
-    }
+	public long getLastOptInStamp() {
+		return lastOptInStamp;
+	}
 
-    public void setLastOptInStamp(long lastOptInStamp) {
-	this.lastOptInStamp = lastOptInStamp;
-    }
+	public void setLastOptInStamp(long lastOptInStamp) {
+		this.lastOptInStamp = lastOptInStamp;
+	}
 
-    public Long getCreatedStamp() {
-	return createdStamp;
-    }
+	public Long getCreatedStamp() {
+		return createdStamp;
+	}
 
-    public void setCreatedStamp(Long createdStamp) {
-	this.createdStamp = createdStamp;
-    }
+	public void setCreatedStamp(Long createdStamp) {
+		this.createdStamp = createdStamp;
+	}
 
-    public String getCreatedBy() {
-	return createdBy;
-    }
+	public String getCreatedBy() {
+		return createdBy;
+	}
 
-    public void setCreatedBy(String createdBy) {
-	this.createdBy = createdBy;
-    }
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
 
-    public String getSessionId() {
-	return sessionId;
-    }
+	public String getSessionId() {
+		return sessionId;
+	}
 
-    public void setSessionId(String sessionId) {
-	this.sessionId = sessionId;
-    }
+	public void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+	}
 
-    public String getChannelType() {
-	return channelType;
-    }
+	public String getChannelType() {
+		return channelType;
+	}
 
-    public void setChannelType(String channelType) {
-	this.channelType = channelType;
-    }
+	public void setChannelType(String channelType) {
+		this.channelType = channelType;
+	}
+
+	public Boolean getEmailVerified() {
+		return emailVerified;
+	}
+
+	public void setEmailVerified(Boolean emailVerified) {
+		this.emailVerified = emailVerified;
+	}
+
+	public Boolean getPhoneVerified() {
+		return phoneVerified;
+	}
+
+	public void setPhoneVerified(Boolean phoneVerified) {
+		this.phoneVerified = phoneVerified;
+	}
 
 }
