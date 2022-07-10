2a300befed0ab78b8f8e2a454ab4f33f3f0d8d4e
lalit.tanwar <lalit.tanwar07@gmail.com>
2022-07-10 15:02:38 +0530

    app fornt added

--- server-account/src/main/java/com/boot/jx/account/api/FrontController.java
+++ server-account/src/main/java/com/boot/jx/account/api/FrontController.java
@@ -130,6 +130,7 @@ public class FrontController {
 			return pmCommonConfig.mainDomainRedirect();
 		}
 		String domainName = ArgUtil.nonEmpty(commonHttpRequest.get("domain"), commonHttpRequest.getSubDomain());
-		return domainProfile(model, domainName, Tenants.isDefault(domainName), "page");
+		String page = Tenants.isDefault(domainName) ? "front" : "page";
+		return domainProfile(model, domainName, Tenants.isDefault(domainName), page);
 	}
 }
