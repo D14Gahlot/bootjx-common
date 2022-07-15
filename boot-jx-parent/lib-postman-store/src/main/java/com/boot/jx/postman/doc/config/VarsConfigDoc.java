7772faba37344af41c2dd4a93e7eb8a02ac9c46c
lalit.tanwar <lalit.tanwar07@gmail.com>
2022-07-15 17:57:18 +0530

    added token keys

---
+++ boot-jx-parent/lib-postman-store/src/main/java/com/boot/jx/postman/doc/config/VarsConfigDoc.java
@@ -0,0 +1,67 @@
+package com.boot.jx.postman.doc.config;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.TypeAlias;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.boot.jx.postman.PMEnvironment.PMConfigurationObject;
+import com.boot.jx.postman.PMEnvironment.ProtectedProperty;
+import com.boot.jx.postman.PMEnvironment.PublicProperty;
+import com.boot.utils.ArgUtil;
+import com.fasterxml.jackson.annotation.JsonView;
+
+@Document(collection = "CONFIG_VARS")
+@TypeAlias("VarsConfigDoc")
+public class VarsConfigDoc extends PMConfigurationObject {
+
+	private static final long serialVersionUID = -4251710793999219993L;
+
+	@Id
+	private String id;
+
+	private String group;
+
+	private String type;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getGroup() {
+		return ArgUtil.nonEmpty(this.group, "default");
+	}
+
+	public void setGroup(String group) {
+		this.group = group;
+	}
+
+	@Document(collection = "CONFIG_COMP_VARS")
+	@TypeAlias("CompanyVarsConfig")
+	public static class CompanyVarsConfigDoc extends VarsConfigDoc {
+		private static final long serialVersionUID = -782517850590239631L;
+
+	}
+
+	public static interface ProtectValue {
+		@JsonView(ProtectedProperty.class)
+		public Object getValue();
+	}
+
+	@Document(collection = "CONFIG_TOKEN_KEYS")
+	@TypeAlias("CompanyTokenKey")
+	public static class CompanyTokenKeyDoc extends VarsConfigDoc implements ProtectValue {
+		private static final long serialVersionUID = -4251710793999219993L;
+	}
+}
