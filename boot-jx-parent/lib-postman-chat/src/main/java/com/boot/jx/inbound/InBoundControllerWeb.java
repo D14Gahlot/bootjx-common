045acd5c79931f02473ab165e75687c6cfa63238
lalit.tanwar <lalit.tanwar07@gmail.com>
2022-08-15 08:26:41 +0530

    inbound push removed

--- boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/inbound/InBoundControllerWeb.java
+++ boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/inbound/InBoundControllerWeb.java
@@ -325,8 +325,8 @@ public class InBoundControllerWeb {
 				InboxMessage sessionMessage = new InboxMessage();
 
 				messageBoxEvent.getInboxMessages().forEach(inboxMessage -> {
-					//inBoundService.invokeMethods(inboxMessage);
-					inboundBottler.push(inboxMessage);
+					inBoundService.invokeMethods(inboxMessage);
+					//inboundBottler.push(inboxMessage);
 					sessionMessage.setSessionId(inboxMessage.getSessionId());
 					sessionMessage.setContact(sessionMessage.getContact());
 				});
