482269b7933fee23ff63eb1563603ab565da3361
lalit.tanwar <lalit.tanwar07@gmail.com>
2022-08-17 17:25:08 +0530

    bot meta fixed

--- boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/chat/ChatService.java
+++ boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/chat/ChatService.java
@@ -272,11 +272,10 @@ public class ChatService {
 				|| !ArgUtil.is(doc.getMeta().getRoutingId(), inboxMessage.session().getRoutingId())) {
 			LOGGER.debug("Loading chat conewxt:newSession");
 			chatLogger.trace(inboxMessage, "NewSession", doc.getMeta(), inboxMessage.session());
-			doc.setMeta(new ChatMeta());
+			messageContext.chat().setMeta(new ChatMeta());
 			messageContext.chat().setQueueCode(inboxMessage.session().getQueue());
 			messageContext.chat().setSessionId(inboxMessage.getSessionId());
 			messageContext.chat().setRoutingId(inboxMessage.session().getRoutingId());
-			messageContext.commitChatContactQuery();
 		} else {
 			chatLogger.trace(inboxMessage, "ContinueOldSession", doc.getMeta());
 		}
