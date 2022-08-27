dfe90844ddc3f8fc832823a8b327d065fd5ef099
lalit.tanwar <lalit.tanwar07@gmail.com>
2022-08-28 03:45:00 +0530

    throttleing

--- boot-jx-parent/lib-mcq/src/main/java/com/boot/jx/zqueue/ZQueueImpl.java
+++ boot-jx-parent/lib-mcq/src/main/java/com/boot/jx/zqueue/ZQueueImpl.java
@@ -34,7 +34,7 @@ public class ZQueueImpl implements ZQueue {
 
 	@Async
 	public void pushBackAsync(TunnelTask task) {
-		this.zQueueEngine.throttle(task);
+		this.zQueueEngine.throttle(new TunnelTask().name(task.getName()).id(task.getId()).intervalSeconds(1));
 	}
 
 }
