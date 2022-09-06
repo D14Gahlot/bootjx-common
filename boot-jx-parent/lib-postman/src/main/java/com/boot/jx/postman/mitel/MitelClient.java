c942b32283a099c46db0a63fc6df9e22bc65f748
lalit.tanwar <lalit.tanwar07@gmail.com>
2022-09-06 15:19:34 +0530

    as string

--- boot-jx-parent/lib-postman/src/main/java/com/boot/jx/postman/mitel/MitelClient.java
+++ boot-jx-parent/lib-postman/src/main/java/com/boot/jx/postman/mitel/MitelClient.java
@@ -40,9 +40,11 @@ public class MitelClient {
 
 		Ajax ajax = restService.ajax(endPoint).path("/AuthorizationServer/Token").field("grant_type", grantType);
 
+		MapModel props = MapModel.from(defaultClient.props());
+
 		if (ArgUtil.areEqual(grantType, "password")) {
-			String username = ArgUtil.parseAsString(defaultClient.props().get("username"));
-			String password = ArgUtil.parseAsString(defaultClient.secret().get("password"));
+			String username = props.keyEntry("username").asString(props.getString("client_id"));
+			String password = props.keyEntry("password").asString(props.getString("client_secret"));
 			ajax.field("username", username).field("password", password);
 		} else {
 			String clientId = ArgUtil.parseAsString(defaultClient.props().get("client_id"));
