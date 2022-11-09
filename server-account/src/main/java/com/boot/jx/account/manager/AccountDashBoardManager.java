0012aef5bc43442a8117745682bec23a4898e6ec
rabiluddin <rabiluddin@gmail.com>
2022-11-10 00:08:15 +0300

    admin/c-panel api finetune

--- server-account/src/main/java/com/boot/jx/account/manager/AccountDashBoardManager.java
+++ server-account/src/main/java/com/boot/jx/account/manager/AccountDashBoardManager.java
@@ -482,14 +482,15 @@ public class AccountDashBoardManager {
 					String formattedDateHm = sdfH.format(date);
 					SimpleDateFormat sdfm = new SimpleDateFormat("mm");
 					String min = sdfm.format(date);
-					int m = Integer.parseInt(min);
-					long tStamp = timeStamp;
-					if (m > 30) {
-						tStamp = timeStamp - ((m - 30) * 60 * 1000L);
-					} else {
-						tStamp = timeStamp - (m * 60 * 1000L);
-					}
-					long tStampWmS = (tStamp - (tStamp % (1000 * 60)));
+					//int m = Integer.parseInt(min);
+					//long tStamp = timeStamp;
+//					if (m > 30) {
+//						tStamp = timeStamp + ((60 - m) * 60 * 1000L);
+//					} else {
+//						tStamp = timeStamp + ((30-m) * 60 * 1000L);
+//					}
+//					long tStampWmS = (tStamp - (tStamp % (1000 * 60)));
+					long tStampWmS = getHour(timeStamp);
 
 					hourListH.add(formattedDateHm);
 					hrDto.setDate(yyyyMMdd);
@@ -681,11 +682,17 @@ public class AccountDashBoardManager {
 		String min = sdfm.format(date);
 		int m = Integer.parseInt(min);
 		long tStamp = timeStamp;
+//		if (m > 30) {
+//			tStamp = timeStamp - ((m - 30) * 60 * 1000L);
+//		} else {
+//			tStamp = timeStamp - (m * 60 * 1000L);
+//		}
 		if (m > 30) {
-			tStamp = timeStamp - ((m - 30) * 60 * 1000L);
+			tStamp = timeStamp + ((60 - m) * 60 * 1000L);
 		} else {
-			tStamp = timeStamp - (m * 60 * 1000L);
+			tStamp = timeStamp + ((30-m) * 60 * 1000L);
 		}
+		
 		long tStampWmS = (tStamp - (tStamp % (1000 * 60)));
 
 		return tStampWmS;
@@ -708,13 +715,18 @@ public class AccountDashBoardManager {
 		Date date = new Date(lastTimeStamp);
 		SimpleDateFormat sdfHM = new SimpleDateFormat("mm");
 		String formattedHM = sdfHM.format(date);
-		long lastTimeStampWm = lastTimeStamp - Integer.parseInt(formattedHM) * 60 * 1000L;
+		int m =Integer.parseInt(formattedHM);
+		if(m>30) {
+			m = 60-m;
+		}else {
+			m = 30-m;
+		}
+	    long currTimeStM=currentTStamp+(m * 60 * 1000L);
+		long lastTimeStampWm = lastTimeStamp +m * 60 * 1000L;
 		lastTimeStampWm = (lastTimeStampWm - (lastTimeStampWm % (1000 * 60)));
-
-		for (long lastTS = lastTimeStampWm; lastTS <= currentTStamp; lastTS = lastTS + DateUtil.MIN_30) {
+		for (long lastTS = lastTimeStampWm; lastTS <= currTimeStM; lastTS = lastTS + DateUtil.MIN_30) {
 			mapMinWise.put(lastTS, new Long(0));
 		}
-
 		Map<Object, Long> result = new TreeMap<Object, Long>(mapMinWise);
 
 		return result;
--- server-admin/src/main/java/com/boot/jx/admin/manager/AdminDashBoardManager.java
+++ server-admin/src/main/java/com/boot/jx/admin/manager/AdminDashBoardManager.java
@@ -1162,17 +1162,7 @@ public class AdminDashBoardManager {
 					Date date = new Date(timeStamp);
 					SimpleDateFormat sdfH = new SimpleDateFormat(DateUtil.DEFAULT_DATE_TIME_FORMAT);
 					String formattedDateHm = sdfH.format(date);
-					SimpleDateFormat sdfm = new SimpleDateFormat("mm");
-					String min = sdfm.format(date);
-					int m = Integer.parseInt(min);
-					long tStamp = timeStamp;
-					if (m > 30) {
-						tStamp = timeStamp - ((m - 30) * 60 * 1000L);
-					} else {
-						tStamp = timeStamp - (m * 60 * 1000L);
-					}
-					long tStampWmS = (tStamp - (tStamp % (1000 * 60)));
-
+					long tStampWmS = getHour(timeStamp);
 					hourListH.add(formattedDateHm);
 					hrDto.setDate(yyyyMMdd);
 					hrDto.setHour(formattedDateHm);
@@ -1364,9 +1354,9 @@ public class AdminDashBoardManager {
 		int m = Integer.parseInt(min);
 		long tStamp = timeStamp;
 		if (m > 30) {
-			tStamp = timeStamp - ((m - 30) * 60 * 1000L);
+			tStamp = timeStamp + ((60 - m) * 60 * 1000L);
 		} else {
-			tStamp = timeStamp - (m * 60 * 1000L);
+			tStamp = timeStamp + ((30-m) * 60 * 1000L);
 		}
 		long tStampWmS = (tStamp - (tStamp % (1000 * 60)));
 
@@ -1385,41 +1375,27 @@ public class AdminDashBoardManager {
 
 	public Map<Object, Long> getHourRange(long currentTStamp, long lastTimeStamp) {
 		Map<String, Long> mapHr = new HashMap<>();
-
 		Map<Object, Long> mapMinWise = new HashMap<>();
 		Date date = new Date(lastTimeStamp);
 		SimpleDateFormat sdfHM = new SimpleDateFormat("mm");
 		String formattedHM = sdfHM.format(date);
-		long lastTimeStampWm = lastTimeStamp - Integer.parseInt(formattedHM) * 60 * 1000L;
+		int m =Integer.parseInt(formattedHM);
+		if(m>30) {
+			m = 60-m;
+		}else {
+			m = 30-m;
+		}
+	    long currTimeStM=currentTStamp+(m * 60 * 1000L);
+		long lastTimeStampWm = lastTimeStamp +m * 60 * 1000L;
 		lastTimeStampWm = (lastTimeStampWm - (lastTimeStampWm % (1000 * 60)));
-
-		/** adding Hour to last time **/
-		/*
-		 * String curHr = getHour(currentTStamp); String lastHr =
-		 * getHour(lastTimeStamp);
-		 * 
-		 * int currHrInt = Integer.parseInt(curHr); int lastHrInt =
-		 * Integer.parseInt(lastHr); if (currHrInt < 12) { currHrInt = currHrInt + 24; }
-		 * for (int i = lastHrInt; i <= currHrInt; i++) { int k = i; if (i > 24) { k = i
-		 * - 24; } String keyS = String.valueOf(k); if (keyS.length() == 1) { keyS = "0"
-		 * + keyS; } mapHr.put(keyS, new Long(0));
-		 * 
-		 * }
-		 * 
-		 * Map<String, Long> result =
-		 * mapHr.entrySet().stream().sorted(Map.Entry.comparingByKey()).collect(
-		 * Collectors .toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue,
-		 * newValue) -> oldValue, LinkedHashMap::new));
-		 */
-
-		for (long lastTS = lastTimeStampWm; lastTS <= currentTStamp; lastTS = lastTS + DateUtil.MIN_30) {
+		for (long lastTS = lastTimeStampWm; lastTS <= currTimeStM; lastTS = lastTS + DateUtil.MIN_30) {
 			mapMinWise.put(lastTS, new Long(0));
 		}
-
 		Map<Object, Long> result = new TreeMap<Object, Long>(mapMinWise);
 
 		return result;
 	}
+
 
 	/** Read ,Unread,sent,deliver msg count Hour Wise */
 
