5f084a199017ab838acd29fda15715fdb9007f56
ninjabase8085 <pranjal.3vyas@gmail.com>
2022-11-30 11:53:32 +0530

    Login Token Changes

--- server-contak/src/main/java/com/boot/jx/contak/api/NotpController.java
+++ server-contak/src/main/java/com/boot/jx/contak/api/NotpController.java
@@ -82,14 +82,17 @@ public class NotpController {
 		if (!ArgUtil.is(loginDTO.phone)) {
 			ApiResponseUtil.throwMissinInputException(new ApiFieldError().field("phone"));
 		}
+		
+		String loginToken = String.valueOf(System.currentTimeMillis());
 
 		PhoneUserDoc userDoc = commonMongoTemplate.findById(loginDTO.phone, PhoneUserDoc.class);
 		if (!ArgUtil.is(userDoc)) {
 			userDoc = new PhoneUserDoc();
 			userDoc.phoneId = loginDTO.phone;
+			userDoc.loginToken = loginToken;
 			commonMongoTemplate.save(userDoc);
 		}
-
+		
 		PhoneUserQuery phoneUserQuery = new PhoneUserQuery(userDoc);
 		PhoneLoginResponseDTO resp = new PhoneLoginResponseDTO();
 		if (ArgUtil.is(loginDTO.deviceToken)) { // Step 3
@@ -105,9 +108,11 @@ public class NotpController {
 				ApiResponseUtil.throwInputException(ApiStatusCodes.PARAM_INVALID, new ApiFieldError().field("otp"));
 			}
 			resp.deviceToken = UniqueID.generateSessionId();
+			resp.loginToken = loginToken;
+			
 			phoneUserQuery.setOtpHash(Constants.BLANK);
 			phoneUserQuery.setOtpNounce(Constants.BLANK);
-			phoneUserQuery.setLoginToken(String.valueOf(System.currentTimeMillis()));
+			phoneUserQuery.setLoginToken(resp.loginToken);
 			phoneUserQuery.setAuthToken(CryptoUtil.getEncoder().message(resp.deviceToken).sha2().toString());
 			commonMongoTemplate.update(phoneUserQuery);
 			return ApiResponse.buildResults(phoneBookManager.getProfile(userDoc), resp);
