283f72ced83c313287963b358492d406d75c2c2e
ninjabase8085 <pranjal.3vyas@gmail.com>
2022-11-30 16:42:08 +0530

    Login Token Changes

--- server-contak/src/main/java/com/boot/jx/contak/api/NotpController.java
+++ server-contak/src/main/java/com/boot/jx/contak/api/NotpController.java
@@ -177,7 +177,7 @@ public class NotpController {
 		String loginToken = userDoc.getLoginToken();
 		if(msg.loginToken != null) {
 			if(msg.loginToken != loginToken) {
-				ApiResponseUtil.throwInputException(ApiStatusCodes.HANDSHAKE_REQUIRED, new ApiFieldError().field("userLoginToken : "+msg.loginToken));
+				ApiResponseUtil.throwInputException(ApiStatusCodes.HANDSHAKE_REQUIRED, new ApiFieldError().field("userLoginToken : "+msg.loginToken+" "+loginToken));
 			}
 		}
 		
