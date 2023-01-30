76053f3bcf3ec769a08e36286f7179ebd6e37750
rabiluddin <rabiluddin@gmail.com>
2023-01-30 07:37:27 +0300

    event

--- server-admin/src/main/java/com/boot/jx/admin/api/AdminDashBoardContoller.java
+++ server-admin/src/main/java/com/boot/jx/admin/api/AdminDashBoardContoller.java
@@ -20,6 +20,7 @@ import com.boot.jx.admin.dto.WabaSummaryDocDto;
 import com.boot.jx.admin.manager.AdminDashBoardManager;
 import com.boot.jx.admin.manager.AgentAnalyticsManager;
 import com.boot.jx.api.ApiResponse;
+import com.boot.jx.api.EventCountSummary;
 import com.boot.jx.postman.doc.ChatSessionDoc;
 
 @RestController
@@ -118,6 +119,15 @@ public class AdminDashBoardContoller {
 		List<WabaSummaryDocDto> summary = adminDbMgr.wabaSummary(timestamp);
 		return ApiResponse.buildResults(summary);
 	}
+	
+	@ResponseBody
+	@RequestMapping(value = { "/admin/event-summary" }, method = { RequestMethod.GET })
+	public ApiResponse<EventCountSummary, Object> getEventCountSummary(String dateRange1, String dateRange2,
+			int days) {
+		EventCountSummary summary = adminDbMgr.getEventCountSummary(dateRange1, dateRange2, days);
+		return ApiResponse.buildResult(summary);
+	}
+
 
 
 }
