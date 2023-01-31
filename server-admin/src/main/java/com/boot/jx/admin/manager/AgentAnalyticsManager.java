ab01105cd53aa1c39f85ea23334b5759cf4f71d4
rabiluddin <rabiluddin@gmail.com>
2023-01-31 18:42:23 +0300

    satisfactionscore null check

--- server-admin/src/main/java/com/boot/jx/admin/manager/AgentAnalyticsManager.java
+++ server-admin/src/main/java/com/boot/jx/admin/manager/AgentAnalyticsManager.java
@@ -777,6 +777,7 @@ public class AgentAnalyticsManager {
 		query.addCriteria(Criteria.where("mode").is("BOT"));
 		query.addCriteria(Criteria.where("assignedToQueue").is("feedback"));
 		query.addCriteria(Criteria.where("assignedToAgent").is(agent));
+		query.addCriteria(Criteria.where("feedback").is(true));
 		query.addCriteria(Criteria.where("startSessionStamp").gt(dateRange1).lt(dateRange2));
 		removeChatSessFieldForSatisScore(query);
 		List<ChatSessionDoc> botLst = mongoTemplate.find(query, ChatSessionDoc.class, CHAT_SESSION);
