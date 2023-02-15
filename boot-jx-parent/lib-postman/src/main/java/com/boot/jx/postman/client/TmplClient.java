f43ea0d7044f5c1af03d069db4d05f1dc43279a1
lalit.tanwar <lalit.tanwar07@gmail.com>
2023-02-16 02:39:14 +0530

    Added location

--- boot-jx-parent/lib-postman/src/main/java/com/boot/jx/postman/client/TmplClient.java
+++ boot-jx-parent/lib-postman/src/main/java/com/boot/jx/postman/client/TmplClient.java
@@ -105,8 +105,7 @@ public class TmplClient {
 		options.put("buttons", buttons);
 
 		Attachment defaultAttachment = optionsModel.keyEntry("attachment").as(Attachment.class);
-		if (ArgUtil.is(defaultAttachment) &&
-				outboxMessage.attachments().size() == 0) {
+		if (ArgUtil.is(defaultAttachment) && outboxMessage.attachments().size() == 0) {
 			outboxMessage.attachments().add(defaultAttachment);
 		}
 
