0d6a259e9617156d9dee835dd9b492105061a2eb
lalit.tanwar <lalit.tanwar07@gmail.com>
2023-04-05 00:13:39 +0530

    builder in bot

--- boot-jx-parent/common-lib/src/main/java/com/boot/jx/app/CommonAppLauncher.java
+++ boot-jx-parent/common-lib/src/main/java/com/boot/jx/app/CommonAppLauncher.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 
@@ -30,7 +31,26 @@ public class CommonAppLauncher extends SpringBootServletInitializer {
 		}
 
 		// System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
-		SpringApplication.run(applicationClass, args);
-		return null;
+		return SpringApplication.run(applicationClass, args);
+	}
+
+	public static ConfigurableApplicationContext build(Class<? extends CommonAppLauncher> applicationClass,
+			String[] args) throws IOException {
+
+		String isLocalTrustStore = ArgUtil.parseAsString(System.getenv(APP_TRUST_STORE_LOCAL), "false");
+
+		if ("false".equalsIgnoreCase(isLocalTrustStore)) {
+			System.out.println("No Local Certificate");
+		} else if ("true".equalsIgnoreCase(isLocalTrustStore)) {
+			String userDir = System.getProperty("user.dir");
+			File userDirFile = new File(userDir);;
+			File userDirFolder = userDirFile.getParentFile();
+			System.setProperty("javax.net.ssl.trustStore", userDirFolder + "/certs/cacerts");
+		} else {
+			System.setProperty("javax.net.ssl.trustStore", isLocalTrustStore);
+		}
+
+		// System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
+		return new SpringApplicationBuilder(applicationClass).run(args);
 	}
 }
--- ms-postman/src/main/java/com/boot/jx/postman/PostManApplication.java
+++ ms-postman/src/main/java/com/boot/jx/postman/PostManApplication.java
@@ -1,6 +1,7 @@
 package com.boot.jx.postman;
 
-import org.springframework.boot.SpringApplication;
+import java.io.IOException;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
@@ -10,41 +11,44 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.context.request.RequestContextListener;
 
+import com.boot.jx.app.CommonAppLauncher;
+
 /**
  * The Class PostManApplication.
  */
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.boot.jx" })
 @EnableAsync(proxyTargetClass = true)
-public class PostManApplication {
+public class PostManApplication extends CommonAppLauncher {
 
-    /**
-     * The main method.
-     *
-     * @param args the arguments
-     */
-    public static void main(String[] args) {
-	SpringApplication.run(PostManApplication.class, args);
-    }
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		launch(PostManApplication.class, args);
+	}
 
-    /**
-     * Security filter chain registration.
-     *
-     * @param securityProperties the security properties
-     * @return the delegating filter proxy registration bean
-     */
-    @Bean
-    @ConditionalOnBean(name = "checkSession")
-    public DelegatingFilterProxyRegistrationBean securityFilterChainRegistration(
-	    SecurityProperties securityProperties) {
-	DelegatingFilterProxyRegistrationBean registration = new DelegatingFilterProxyRegistrationBean("checkSession");
-	registration.setOrder(securityProperties.getFilter().getOrder());
-	return registration;
-    }
+	/**
+	 * Security filter chain registration.
+	 *
+	 * @param securityProperties the security properties
+	 * @return the delegating filter proxy registration bean
+	 */
+	@Bean
+	@ConditionalOnBean(name = "checkSession")
+	public DelegatingFilterProxyRegistrationBean securityFilterChainRegistration(
+			SecurityProperties securityProperties) {
+		DelegatingFilterProxyRegistrationBean registration = new DelegatingFilterProxyRegistrationBean("checkSession");
+		registration.setOrder(securityProperties.getFilter().getOrder());
+		return registration;
+	}
 
-    @Bean
-    public RequestContextListener requestContextListener() {
-	return new RequestContextListener();
-    }
-    
+	@Bean
+	public RequestContextListener requestContextListener() {
+		return new RequestContextListener();
+	}
+
 }
