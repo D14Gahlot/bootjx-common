93bf2c028b35925c6c1de3c23b425c0cc2abe66f
lalit.tanwar <lalit.tanwar07@gmail.com>
2023-05-24 16:08:21 +0530

    proxy apis

--- server-xms/src/main/java/com/boot/jx/xms/api/ProxyController.java
+++ server-xms/src/main/java/com/boot/jx/xms/api/ProxyController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +33,7 @@ public class ProxyController {
 	@Autowired
 	ProxyService service;
 
+	@CrossOrigin(origins = "*")
 	@ApiRequest(type = RequestType.NO_TRACK_PING)
 	@ApiOperation(value = "Try API's", hidden = true)
 	@RequestMapping(value = { "/proxy/{domainHash}", "/proxy/{domainHash}/*", "/proxy/{domainHash}/**" },
