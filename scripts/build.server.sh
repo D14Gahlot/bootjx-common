f8bb0287654e5cd36d102d838e5e29fe66fec3c4
lalit.tanwar <lalit.tanwar07@gmail.com>
2023-07-03 15:19:16 +0530

    server script

---
+++ scripts/build.server.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#source config/setvars.sh
+source ~/.bash_profile
+
+#bash setup.sh
+
+#mvn clean
+
+# short command
+mvn clean package -pl $1 -am -DskipTests  $2 $3 $4 $5 $6
+
+#java -jar $1/target/$1-0.0.1-SNAPSHOT.jar $2 $3 $4 $5 $6
