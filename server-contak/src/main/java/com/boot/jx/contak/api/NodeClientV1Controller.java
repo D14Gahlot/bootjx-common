eb7d4cef97d5200f8c67aa0e75dd7872c8e5e77e
lalit.tanwar <lalit.tanwar07@gmail.com>
2023-08-26 01:08:56 +0530

    Node3 Controller

--- server-contak/src/main/java/com/boot/jx/contak/api/NodeClientV1Controller.java
+++ server-contak/src/main/java/com/boot/jx/contak/api/NodeClientV1Controller.java
@@ -1,20 +1,14 @@
 package com.boot.jx.contak.api;
 
-import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
-import com.boot.jx.AppContextUtil;
 import com.boot.jx.api.ApiFieldError;
 import com.boot.jx.api.ApiResponse;
 import com.boot.jx.api.ApiResponseUtil;
@@ -35,7 +29,6 @@ import com.boot.jx.contak.manager.UserRegistrationManager;
 import com.boot.jx.exception.ApiHttpExceptions.ApiStatusCodes;
 import com.boot.jx.filter.AppRequestUtil;
 import com.boot.jx.http.ApiRequest;
-import com.boot.jx.model.CommonFile;
 import com.boot.jx.mongo.CommonDocInterfaces.TimeStampIndex;
 import com.boot.jx.mongo.CommonMongoQueryBuilder;
 import com.boot.jx.mongo.CommonMongoTemplate;
@@ -62,12 +55,6 @@ public class NodeClientV1Controller {
 
 	@Autowired
 	private UserRegistrationManager userRegistrationManager;
-
-	@Autowired
-	private ContakInboundRouter inboundManager;
-
-	@Autowired
-	private AWSFileStore fileStore;
 
 	@ApiRequest(authenticateTenant = true)
 	@ApiMockParams({ @ApiMockParam(name = ParamKeys.X_API_KEY, value = "API Key", paramType = MockParamType.HEADER) })
@@ -216,62 +203,4 @@ public class NodeClientV1Controller {
 		return ApiResponse.buildResults(tmpl);
 	}
 
-	@ApiRequest(authenticateTenant = true)
-	@RequestMapping(value = { "/api/v1/org/{companyId}/hsm/tmpl" }, method = { RequestMethod.GET })
-	public ApiResponse<ContakTemplateDoc, Object> hsmTemplate(Model model) throws NoSuchAlgorithmException {
-		CompanyDoc compoc = apiContext.validateCompany();
-		return ApiResponse.buildResults(commonMongoTemplate.collection(ContakTemplateDoc.class)
-				.where("companyId", compoc.companyId).find().asList());
-	}
-
-	@ApiRequest(authenticateTenant = true)
-	@RequestMapping(value = { "/api/v1/org/{companyId}/hsm/tmpl" }, method = { RequestMethod.POST })
-	public ApiResponse<ContakTemplateDoc, Object> hsmTemplate(Model model, @RequestBody ContakTemplateDoc template)
-			throws NoSuchAlgorithmException {
-		CompanyDoc compoc = apiContext.validateCompany();
-
-		if (ArgUtil.is(template.templateId)) {
-			ApiResponseUtil.throwInputException(new ApiFieldError().field("templateId").codeKey("AccessDenied")
-					.description("Template Cannot be modified"));
-		}
-
-		if (!ArgUtil.is(template.companyId)) {
-			ApiResponseUtil.throwInputException(
-					new ApiFieldError().field("companyId").codeKey("AccessDenied").description("Select Organization"));
-		}
-
-		template.templateId = String.format("%s:%s", template.companyId, template.code);
-		commonMongoTemplate.save(template);
-		return ApiResponse.buildResult(template);
-	}
-
-	@ApiRequest(authenticateTenant = true)
-	@RequestMapping(value = { "/api/v1/org/{companyId}/hsm/tmpl/{templateId}" }, method = { RequestMethod.DELETE })
-	public ApiResponse<Object, Object> hsmTemplate(Model model, @PathVariable String companyId,
-			@PathVariable String templateId) throws NoSuchAlgorithmException {
-		CompanyDoc compoc = apiContext.validateCompany();
-		ContakTemplateDoc template = commonMongoTemplate.collection(ContakTemplateDoc.class)
-				.with(Criteria.where("companyId").is(companyId).and("templateId").is(templateId)).find().asFirst();
-		template.setDeleted(!template.isDeleted());
-		commonMongoTemplate.saveAndAudit(template);
-		return ApiResponse.build().message("Template has been " + (template.isDeleted() ? "deleted" : "restored"));
-	}
-
-	@ApiRequest(authenticateTenant = true)
-	@RequestMapping(value = "/api/v1/org/{companyId}/hsm/tmpl/{templateId}/media", method = { RequestMethod.POST })
-	public ApiResponse<CommonFile, Object> uploadFile(@RequestParam(name = "file", required = false) MultipartFile file,
-			@RequestParam(name = "thumbnail", required = false) MultipartFile thumbnail, @PathVariable String companyId,
-			@PathVariable String templateId) {
-		CompanyDoc compoc = apiContext.validateCompany();
-		CommonFile f = fileStore.upload1(file,
-				String.format("%s_%s/tmpl/%s", AppContextUtil.getTenant(), companyId, templateId),
-				file.getOriginalFilename());
-		if (ArgUtil.is(thumbnail)) {
-			CommonFile thumb = fileStore.upload1(thumbnail,
-					String.format("%s_%s/tmpl/%s/th", AppContextUtil.getTenant(), companyId, templateId),
-					file.getOriginalFilename());
-			f.setThumb(thumb.getUrl());
-		}
-		return ApiResponse.buildResults(f).message("Header Uplodaed");
-	}
 }
