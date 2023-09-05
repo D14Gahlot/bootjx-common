077aeed907b3b1b9c4472c28c594e6d0714fc0fd
lalit.tanwar <lalit.tanwar07@gmail.com>
2023-09-06 04:04:41 +0530

    demo default

--- boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/inbound/InBoundControllerTest.java
+++ boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/inbound/InBoundControllerTest.java
@@ -73,8 +73,7 @@ public class InBoundControllerTest {
 		}
 
 		if (ArgUtil.is(bootJxConfigService)) {
-			model.addAllAttributes(bootJxConfigService.bootJxAttributesModel().cdnApp("test").cdnAEntry("dev")
-					.cdnStatic("demo").map());
+			model.addAllAttributes(bootJxConfigService.bootJxAttributesModel().cdnApp("test").cdnAEntry("dev").map());
 		}
 
 		return "app-test";
