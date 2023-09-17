74d3f4f22f5f4ce59809ac4c6709e65fb9ffd4c1
rabiluddin <rabiluddin@gmail.com>
2023-09-17 22:18:05 +0300

    for media

--- boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/connectors/WA360CloudConnector.java
+++ boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/connectors/WA360CloudConnector.java
@@ -293,9 +293,9 @@ public class WA360CloudConnector extends AbstractConnector<WA360CloudConfigDetai
 			FileType fileType) {
 		try {
 			WA360InboundMedia media = map.entry(path).as(WA360InboundMedia.class);
-			CommonFileStream srcFile = new CommonFileStream().url(WA360Constants.MEDIA_URL(media.getId()))
+			CommonFileStream srcFile = new CommonFileStream().url(WA360Constants.MEDIA_CLOUD_URL(media.getId()))
 					.fileType(fileType).format(FileFormat.from(media.getMimeType()))
-					.header(WA360Constants.D360_API_KEY, channelConfig.getWa360d().getApiKey())
+					.header(WA360Constants.D360_CLOUD_API_KEY, channelConfig.getWa360dc().getApiKey())
 					.name(ArgUtil.nonEmpty(media.getFilename(), media.getCaption()));
 
 			CommonFile dstFile = pmFileStoreClient.uploadSessionFileAsync(srcFile,
@@ -433,8 +433,8 @@ public class WA360CloudConnector extends AbstractConnector<WA360CloudConfigDetai
 				phone = String.format("+%s", phone);
 			}
 
-			//MapModel resp = wa360CloudClient.fetchContact(phone, channelConfig);
-			//String waId = resp.getString("wa_id");
+			MapModel resp = wa360CloudClient.fetchContact(phone, channelConfig);
+			String waId = resp.getString("wa_id");
 
 			String input =null;// resp.getString("input");
 			String status ="valid";// resp.getString("status");
