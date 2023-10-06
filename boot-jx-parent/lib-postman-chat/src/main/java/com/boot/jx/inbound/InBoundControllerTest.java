abdcf917e4239ccf0fed871d3e8439001bdedfd6
lalit.tanwar <lalit.tanwar07@gmail.com>
2023-10-06 19:16:40 +0530

    upgrade request

--- boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/inbound/InBoundControllerTest.java
+++ boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/inbound/InBoundControllerTest.java
@@ -106,7 +106,8 @@ public class InBoundControllerTest {
 		}
 
 		if (ArgUtil.is(bootJxConfigService)) {
-			model.addAllAttributes(bootJxConfigService.bootJxAttributesModel().cdnApp("test").cdnAEntry("dev").map());
+			model.addAllAttributes(bootJxConfigService.bootJxAttributesModel().cdnApp("test").cdnAEntry("dev")
+					.preventUpgradeInsecureRequest().map());
 		}
 
 		return "app-test";
