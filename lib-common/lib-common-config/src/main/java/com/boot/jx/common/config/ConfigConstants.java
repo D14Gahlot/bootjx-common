6ba3ce342eece6a45204fc1abe17c5a29bdc6b24
deeksha <gudiy@LAPTOP-N1NJ69G3>
2023-12-12 09:13:34 +0530

    cs-1005

--- lib-common/lib-common-config/src/main/java/com/boot/jx/common/config/ConfigConstants.java
+++ lib-common/lib-common-config/src/main/java/com/boot/jx/common/config/ConfigConstants.java
@@ -88,7 +88,7 @@ public class ConfigConstants {
 
 		POSTMAN_CHAT_FEEDBACK_QUEUE(new ConfigMeta("Default Feedback Queue", PROPERTIES.POSTMAN_CHAT_FEEDBACK_QUEUE)
 				.desc("Default Feedback App").optionsSource("getx:/api/options/inbound_queue").optionsKey("code")
-				.optionsLabel("code").filter("type", APP_TYPE.FEEDBACK)),
+				.optionsLabel("code").filter("appType", APP_TYPE.FEEDBACK)),
 
 		POSTMAN_CHAT_CHANNEL_SANDBOX(new ConfigMeta("Enable Sandbox Channels", "postman.chat.channel.sandbox")
 				.desc("Sandbox channels are preconfigured communication channels").optionsOnOff()),
