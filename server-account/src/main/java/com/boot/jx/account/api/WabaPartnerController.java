6a8aa847342d3dc1926911a3d1023112c6a4f748
lalit.tanwar <lalit.tanwar07@gmail.com>
2023-12-26 12:21:00 +0530

    added right error

--- server-account/src/main/java/com/boot/jx/account/api/WabaPartnerController.java
+++ server-account/src/main/java/com/boot/jx/account/api/WabaPartnerController.java
@@ -370,9 +370,13 @@ public class WabaPartnerController {
 				mongoTemplate.save(channelDoc);
 				return ApiResponse.buildResult(resp.toMap());
 			} catch (ApiHttpClientException e) {
-				// e.printStackTrace();
+
+			
+
+				//e.printStackTrace();
 				MapModel error = MapModel.from(e.getResponse().getBody());
-				if (ArgUtil.is(error)) {
+				if(ArgUtil.is(error)) {
+
 					ApiResponseUtil.addError(error.pathEntry("/meta/developer_message").asString());
 				}
 				throw e;
