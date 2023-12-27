9aea66290d9af84ad3100948ba2b491ade6323c3
deeksha <gudiy@LAPTOP-N1NJ69G3>
2023-12-27 16:09:38 +0530

    cs-1082(instagarm and facebook plugin-properties)

--- boot-jx-parent/lib-postman/src/main/java/com/boot/jx/postman/plugin/InstagramPlugin.java
+++ boot-jx-parent/lib-postman/src/main/java/com/boot/jx/postman/plugin/InstagramPlugin.java
@@ -33,10 +33,13 @@ public class InstagramPlugin implements DefaultChannelPlugin<InstagramConfig> {
 	public static class InstagramConfig extends AChannelDetails {
 
 		private static final long serialVersionUID = -2397678752642150000L;
+		@ConfigMetaProperty(path = "instagram.pageId", title = "Instagram Id", createonly = true)
 		private String pageId;
+		@ConfigMetaProperty(path = "instagram.handler", title = "Handle")
 		private String handler;
+		@ConfigMetaProperty(path = "instagram.type", title = "Type", optionValues = "page", hidden = true)
 		private String type;
-		
+
 		@ConfigMetaProperty(path = "instagram.accessToken", title = "Access Token", writeonly = true)
 		@JsonView(PMEnvironment.ProtectedProperty.class)
 		private String accessToken;
@@ -49,10 +52,8 @@ public class InstagramPlugin implements DefaultChannelPlugin<InstagramConfig> {
 
 		@ConfigMetaProperty(path = "instagram.promptEmail", title = "Prompt Email", inputType = INPUT_TYPE.OPTIONS, dataType = DATA_TYPE.SWITCH, converterType = CONVERT_TYPE.BOOLEAN, defaultValue = "false")
 		private boolean promptEmail;
-
 		@ConfigMetaProperty(path = "instagram.promptPhone", title = "Prompt Phone", inputType = INPUT_TYPE.OPTIONS, dataType = DATA_TYPE.SWITCH, converterType = CONVERT_TYPE.BOOLEAN, defaultValue = "false")
 		private boolean promptPhone;
-
 		@ConfigMetaProperty(path = "instagram.promptName", title = "Prompt Name", inputType = INPUT_TYPE.OPTIONS, dataType = DATA_TYPE.SWITCH, converterType = CONVERT_TYPE.BOOLEAN, defaultValue = "false")
 		private boolean promptName;
 
@@ -161,8 +162,7 @@ public class InstagramPlugin implements DefaultChannelPlugin<InstagramConfig> {
 		return config.getInstagram();
 	}
 
-	
-/*	@Override
+	/*@Override
 	public void addConfigMeta(List<ConfigMeta> list) {
 		list.add(new ConfigMeta().path("instagram.pageId").title("Instagram Id").createonly());
 		list.add(new ConfigMeta().path("instagram.type").title("Type").optionValues("page").hidden());
