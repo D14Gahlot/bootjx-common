c9f06d13ac40cd75ad2d0fb4a8e8e201fe45ef29
deeksha <gudiy@LAPTOP-N1NJ69G3>
2023-12-27 15:28:16 +0530

    cs-1082

--- boot-jx-parent/lib-postman/src/main/java/com/boot/jx/postman/plugin/InstagramPlugin.java
+++ boot-jx-parent/lib-postman/src/main/java/com/boot/jx/postman/plugin/InstagramPlugin.java
@@ -36,11 +36,14 @@ public class InstagramPlugin implements DefaultChannelPlugin<InstagramConfig> {
 		private String pageId;
 		private String handler;
 		private String type;
-
+		
+		@ConfigMetaProperty(path = "instagram.accessToken", title = "Access Token", writeonly = true)
 		@JsonView(PMEnvironment.ProtectedProperty.class)
 		private String accessToken;
+		@ConfigMetaProperty(path = "instagram.verifyToken", title = "Verify Token", writeonly = true)
 		@JsonView(PMEnvironment.ProtectedProperty.class)
 		private String verifyToken;
+		@ConfigMetaProperty(path = "instagram.appSecret", title = "App Secret", writeonly = true)
 		@JsonView(PMEnvironment.ProtectedProperty.class)
 		private String appSecret;
 
@@ -158,7 +161,8 @@ public class InstagramPlugin implements DefaultChannelPlugin<InstagramConfig> {
 		return config.getInstagram();
 	}
 
-	@Override
+	
+/*	@Override
 	public void addConfigMeta(List<ConfigMeta> list) {
 		list.add(new ConfigMeta().path("instagram.pageId").title("Instagram Id").createonly());
 		list.add(new ConfigMeta().path("instagram.type").title("Type").optionValues("page").hidden());
@@ -176,7 +180,7 @@ public class InstagramPlugin implements DefaultChannelPlugin<InstagramConfig> {
 		channelDetails.setVerifyToken(map.pathEntry("instagram.verifyToken").asString(channelDetails.getVerifyToken()));
 		channelDetails.setAccessToken(map.pathEntry("instagram.accessToken").asString(channelDetails.getAccessToken()));
 		channelDetails.setAppSecret(map.pathEntry("instagram.appSecret").asString(channelDetails.getAppSecret()));
-	}
+	}*/
 
 	@Override
 	public boolean isPushAllowed() {
