1fb16377f003810105ec78abf9f3c37b5df11e94
lalit.tanwar <lalit.tanwar07@gmail.com>
2024-01-09 20:49:56 +0530

    doc updates

--- boot-jx-parent/lib-postman-store/src/main/java/com/boot/jx/postman/doc/config/ChannelConfigSetupDoc.java
+++ boot-jx-parent/lib-postman-store/src/main/java/com/boot/jx/postman/doc/config/ChannelConfigSetupDoc.java
@@ -1,22 +1,31 @@
 package com.boot.jx.postman.doc.config;
 
+import java.io.Serializable;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import com.boot.jx.AppContextUtil;
-import com.boot.jx.postman.plugin.ChannelConfig;
-import com.boot.utils.ArgUtil;
+import com.boot.jx.dict.ContactType;
+import com.boot.jx.postman.PMConstants.CHANNEL_TYPE_ENUM;
 
 @Document(collection = "CONFIG_CHANNEL_SETUP")
 @TypeAlias("ChannelConfigSetup")
-public class ChannelConfigSetupDoc extends ChannelConfig {
+public class ChannelConfigSetupDoc implements Serializable {
 
 	private static final long serialVersionUID = -6368905475787041196L;
 
+	@Id
+	private String id;
+	private CHANNEL_TYPE_ENUM channelType;
+	private ContactType contactType;
+
+	private String metaAppId;
+	private String metaAppSecret;
+	private String metaAppVerifyToken;
+
 	private String domain;
 
-	@Id
 	public String getId() {
 		return id;
 	}
@@ -25,16 +34,52 @@ public class ChannelConfigSetupDoc extends ChannelConfig {
 		this.id = id;
 	}
 
-	public boolean isReadOnly() {
-		return (this.isSandbox() || this.isShared()) && !ArgUtil.areEqual(domain, AppContextUtil.getTenant());
-	}
-
 	public String getDomain() {
 		return domain;
 	}
 
 	public void setDomain(String domain) {
 		this.domain = domain;
+	}
+
+	public CHANNEL_TYPE_ENUM getChannelType() {
+		return channelType;
+	}
+
+	public void setChannelType(CHANNEL_TYPE_ENUM channelType) {
+		this.channelType = channelType;
+	}
+
+	public ContactType getContactType() {
+		return contactType;
+	}
+
+	public void setContactType(ContactType contactType) {
+		this.contactType = contactType;
+	}
+
+	public String getMetaAppId() {
+		return metaAppId;
+	}
+
+	public void setMetaAppId(String metaAppId) {
+		this.metaAppId = metaAppId;
+	}
+
+	public String getMetaAppSecret() {
+		return metaAppSecret;
+	}
+
+	public void setMetaAppSecret(String metaAppSecret) {
+		this.metaAppSecret = metaAppSecret;
+	}
+
+	public String getMetaAppVerifyToken() {
+		return metaAppVerifyToken;
+	}
+
+	public void setMetaAppVerifyToken(String metaAppVerifyToken) {
+		this.metaAppVerifyToken = metaAppVerifyToken;
 	}
 
 }
