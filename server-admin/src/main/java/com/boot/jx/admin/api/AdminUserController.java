b9554c8546201ae9bb17988fd60540ec513a5ced
lalit.tanwar <lalit.tanwar07@gmail.com>
2024-01-26 07:06:37 +0300

    delete groups
    # Conflicts:
    #	server-admin/src/main/java/com/boot/jx/admin/api/AdminUserController.java

--- server-admin/src/main/java/com/boot/jx/admin/api/AdminUserController.java
+++ server-admin/src/main/java/com/boot/jx/admin/api/AdminUserController.java
@@ -130,11 +130,9 @@ public class AdminUserController {
 	public ApiResponse<GroupReqDto, Object> fetchGroups(@RequestParam(value = "groupId", required = false) String groupId){
 			return ApiResponse.buildResults(adminService.fetchGroups(groupId));
 		}
-	
 	@RequestMapping(value = "/api/delete-group-contacts", method = { RequestMethod.POST })
 	public ApiResponse<GroupReqDto, Object> deleteGroups(@RequestBody GroupReqDto reqDto){
 			return ApiResponse.buildResults(adminService.deleteGroups(reqDto));
 		}
-
 
 }
