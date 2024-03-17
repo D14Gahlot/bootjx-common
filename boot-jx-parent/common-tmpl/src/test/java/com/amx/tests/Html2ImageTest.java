eef6f52dbed72c9d12852dde4918cae7b9e141d7
lalit.tanwar <lalit.tanwar07@gmail.com>
2024-03-17 17:18:43 +0530

    html to image

---
+++ boot-jx-parent/common-tmpl/src/test/java/com/amx/tests/Html2ImageTest.java
@@ -0,0 +1,24 @@
+package com.amx.tests;
+
+import java.io.File;
+
+import org.junit.Test;
+
+
+import com.boot.jx.tmpl.CommonTmpPackageImpl;
+
+public class Html2ImageTest {
+
+	public static CommonTmpPackageImpl ppkg = new CommonTmpPackageImpl();
+
+	public static void main(String[] arg) {
+		new Html2ImageTest().wa360Templates();
+	}
+
+	@Test
+	public void wa360Templates() {
+		// assertEquals("t1", StringUtils.trim("/abc/def/ghij", '/'), "abc/def/ghij");
+
+	}
+
+}
