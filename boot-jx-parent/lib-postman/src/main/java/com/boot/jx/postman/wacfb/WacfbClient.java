23d13e779f9ddb0764c2e6383cc9d6edcb5f3812
lalit.tanwar <lalit.tanwar07@gmail.com>
2024-03-19 13:32:20 +0530

    fixes

--- boot-jx-parent/lib-postman/src/main/java/com/boot/jx/postman/wacfb/WacfbClient.java
+++ boot-jx-parent/lib-postman/src/main/java/com/boot/jx/postman/wacfb/WacfbClient.java
@@ -43,7 +43,7 @@ import com.boot.utils.JsonPath;
 import com.boot.utils.StringUtils;
 
 @Component
-public class WacfbClient2{
+public class WacfbClient{
 	@Autowired
 	private RestService restService;
 
