83d2804e969b8a1ddda753505e3d639478258b35
deeksha <gudiy@LAPTOP-N1NJ69G3>
2024-03-19 12:51:46 +0530

    n

--- boot-jx-parent/lib-postman/src/main/java/com/boot/jx/postman/wacfb/WacfbInboundMedia.java
+++ boot-jx-parent/lib-postman/src/main/java/com/boot/jx/postman/wacfb/WacfbInboundMedia.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class WACFBInboundMedia2 {
+public class WACFBInboundMedia {
 
     @Deprecated
     @ApiMockModelProperty(example = "/file/of/vdo", value = "absolute-filepath-on-coreapp", hidden = true)
