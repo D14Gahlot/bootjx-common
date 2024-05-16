2aad6e828ed0207524411e8555f566685e4018ad
lalit.tanwar <lalit.tanwar07@gmail.com>
2024-05-16 18:43:24 +0530

    apis

--- server-admin/src/main/java/com/boot/jx/admin/api/AdminObjectsController.java
+++ server-admin/src/main/java/com/boot/jx/admin/api/AdminObjectsController.java
@@ -1,5 +1,8 @@
 package com.boot.jx.admin.api;
 
+import java.util.Enumeration;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.boot.jx.api.ApiResponse;
 import com.boot.jx.common.doc.UserActivityLogDoc;
+import com.boot.jx.dict.ContactType;
+import com.boot.jx.http.CommonHttpRequest;
 import com.boot.jx.mongo.CommonDocInterfaces.AuditActivityDoc;
 import com.boot.jx.mongo.CommonMongoQB.MQB;
 import com.boot.jx.mongo.CommonMongoQB.MongoQueryBuilder;
 import com.boot.jx.postman.PMEnvironment;
 import com.boot.jx.postman.doc.MessageDoc.MessageDocLogs;
+import com.boot.jx.postman.doc.config.ChannelConfigTempDoc;
+import com.boot.jx.postman.doc.tpo.PayloadDumpCollection;
 import com.boot.jx.postman.store.MessageStore;
 import com.boot.model.UtilityModels.PublicJsonProperty;
 import com.boot.utils.ArgUtil;
@@ -24,6 +31,40 @@ public class AdminObjectsController {
 
 	@Autowired
 	private MessageStore messageStore;
+
+	@Autowired
+	private CommonHttpRequest commonHttpRequest;
+
+	@SuppressWarnings("unchecked")
+	public <T> List<T> getPaginatedBulk(Class<T> docClass, String collectionName, int pageNo, int pageSize,
+			String sortBy, String sortDir) {
+		MQB<T> q = MongoQueryBuilder.select(docClass, collectionName).page(pageNo, pageSize);
+
+		Enumeration<String> params = commonHttpRequest.getRequest().getParameterNames();
+
+		while (params.hasMoreElements()) {
+			String param = (String) params.nextElement();
+			switch (param) {
+			case "pageSize":
+			case "pageNo":
+			case "sortBy":
+			case "id":
+				break;
+			default:
+				String paramValue = commonHttpRequest.getRequest().getParameter(param);
+				if (ArgUtil.is(paramValue)) {
+					q.where("param").is(paramValue);
+				}
+				break;
+			}
+		}
+
+		if (ArgUtil.is(sortBy)) {
+			q = q.sortBy(sortBy, Direction.fromString(sortDir));
+		}
+
+		return messageStore.find(q);
+	}
 
 	@RequestMapping(value = "/api/objects/logs", method = { RequestMethod.GET })
 	@JsonView(PMEnvironment.PublicProperty.class)
@@ -37,6 +78,7 @@ public class AdminObjectsController {
 		if (ArgUtil.is(sortBy)) {
 			q = q.sortBy(sortBy, Direction.fromString(sortDir));
 		}
+
 		return ApiResponse.buildResults(messageStore.find(q));
 	}
 
@@ -86,5 +128,33 @@ public class AdminObjectsController {
 			q = q.sortBy(sortBy, Direction.fromString(sortDir));
 		}
 		return ApiResponse.buildResults(messageStore.find(q));
+	}
+
+	@RequestMapping(value = "/api/objects/payload_dump", method = { RequestMethod.GET })
+	@JsonView(PublicJsonProperty.class)
+	public ApiResponse<PayloadDumpCollection, Object> getPayLoadDump(@RequestParam(required = false) String id,
+			@RequestParam(required = false, defaultValue = "0") int pageNo,
+			@RequestParam(required = false, defaultValue = "25") int pageSize,
+			@RequestParam(required = false, defaultValue = "createdStamp") String sortBy,
+			@RequestParam(required = false, defaultValue = "desc") String sortDir,
+			@RequestParam(required = false) ContactType contactType, @RequestParam(required = false) String channelType,
+			@RequestParam(required = false) String channelId, @RequestParam(required = false) String type) {
+		return ApiResponse.buildResults(
+				getPaginatedBulk(PayloadDumpCollection.class, "PAYLOAD_DUMP", pageNo, pageSize, sortBy, sortDir));
+	}
+
+	@RequestMapping(value = { "/api/objects/channel_setup_logs", "/pub/objects/channel_setup_logs" },
+			method = { RequestMethod.GET })
+	@JsonView(PublicJsonProperty.class)
+	public ApiResponse<ChannelConfigTempDoc, Object> channelSetupLogs(@RequestParam(required = false) String id,
+			@RequestParam(required = false, defaultValue = "0") int pageNo,
+			@RequestParam(required = false, defaultValue = "25") int pageSize,
+			@RequestParam(required = false, defaultValue = "createdStamp") String sortBy,
+			@RequestParam(required = false, defaultValue = "desc") String sortDir,
+			@RequestParam(required = false) ContactType contactType, @RequestParam(required = false) String channelType,
+			@RequestParam(required = false) String channelId, @RequestParam(required = false) String domain,
+			@RequestParam(required = false) String lane) {
+		return ApiResponse.buildResults(
+				getPaginatedBulk(ChannelConfigTempDoc.class, "TEMP_CONFIG_CHANNEL", pageNo, pageSize, sortBy, sortDir));
 	}
 }
