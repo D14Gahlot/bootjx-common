004d7e7a856437aed6f7a57827ab384784015a3f
lalit.tanwar <lalit.tanwar07@gmail.com>
2024-05-20 12:40:22 +0530

    sso login

--- server-account/src/main/java/com/boot/jx/account/api/UserController.java
+++ server-account/src/main/java/com/boot/jx/account/api/UserController.java
@@ -70,9 +70,10 @@ public class UserController {
 	@ResponseBody
 	@RequestMapping(value = "/pub/login", method = { RequestMethod.POST })
 	public ApiResponse<UserAuthToken, Object> agentLogin(@RequestParam String username, @RequestParam String password,
-			@RequestParam(required = false) String app, @RequestParam String tnt, @RequestParam String domainId,
-			@RequestParam(required = false) String otp, @RequestParam(required = false) String otpNounce,
-			@RequestParam(required = false) String tokenId) throws NoSuchAlgorithmException {
+			@RequestParam(required = false, defaultValue = "sso") String app, @RequestParam String tnt,
+			@RequestParam String domainId, @RequestParam(required = false) String otp,
+			@RequestParam(required = false) String otpNounce, @RequestParam(required = false) String tokenId)
+			throws NoSuchAlgorithmException {
 
 		UserAuthToken loginToken = empAuthService.createAgentLoginToken(username, username, password, tnt, domainId,
 				app, "LOGIN");
@@ -139,9 +140,10 @@ public class UserController {
 	@RequestMapping(value = "/pub/resetpass/{flow}", method = { RequestMethod.POST })
 	public ApiResponse<?, ?> resetPass(@PathVariable String flow, @RequestParam String username,
 			@RequestParam(required = false) String password, @RequestParam(required = false) String newpassword,
-			@RequestParam(required = false) String app, @RequestParam String tnt, @RequestParam String domainId,
-			@RequestParam(required = false) String otp, @RequestParam(required = false) String otpNounce,
-			@RequestParam(required = false) String tokenId) throws NoSuchAlgorithmException {
+			@RequestParam(required = false, defaultValue = "sso") String app, @RequestParam String tnt,
+			@RequestParam String domainId, @RequestParam(required = false) String otp,
+			@RequestParam(required = false) String otpNounce, @RequestParam(required = false) String tokenId)
+			throws NoSuchAlgorithmException {
 
 		if ("FORGOTPASS".equalsIgnoreCase(flow)) {
 			return empAuthService.agentResetPass(username, ArgUtil.is(app, "admin"));
