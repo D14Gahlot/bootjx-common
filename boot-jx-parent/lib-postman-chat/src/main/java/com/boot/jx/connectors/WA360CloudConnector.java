c3bda6e4d420a134f5876862a2ceac87d89c4fae
lalit.tanwar <lalit.tanwar07@gmail.com>
2024-07-25 18:39:49 +0530

    reply title added

--- boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/connectors/WA360CloudConnector.java
+++ boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/connectors/WA360CloudConnector.java
@@ -196,25 +196,22 @@ public class WA360CloudConnector extends AbstractConnector<WA360CloudConfigDetai
 				inboxMessage.form().put("reply_id", replyId);
 				inboxMessage.form().put("reply_title",
 						map.entry(InBoundWrapperPaths.INTERACTIVE_BUTTON_REPLY).asString());
-				inboxMessage.setMessage(ArgUtil.parseAsString(inboxMessage.form().get("reply_title"), Constants.BLANK));
 			} else if ("list_reply".equals(interactiveType)) {
 				replyId = map.entry(InBoundWrapperPaths.INTERACTIVE_LIST_ID).asString();
 				inboxMessage.form().put("reply_id", replyId);
 				inboxMessage.form().put("reply_title",
 						map.entry(InBoundWrapperPaths.INTERACTIVE_LIST_REPLY).asString());
 				inboxMessage.form().put("reply_desc", map.entry(InBoundWrapperPaths.INTERACTIVE_LIST_DESC).asString());
+			} else if ("nfm_reply".equals(interactiveType)) {
+				String responseJsonString = map.entry(InBoundWrapperPaths.INTERACTIVE_NFM_REPLY_RESPONSE_JSON)
+						.asString();
+				// replyJsonMap.put("response_json", responseJsonString);
+				Map<String, Object> replyJsonMap = JsonUtil.fromJsonToMap(responseJsonString);
+				inboxMessage.form().put("reply_json", replyJsonMap);
+				inboxMessage.form().put("reply_title",
+						map.entry(InBoundWrapperPaths.INTERACTIVE_NFM_REPLY_BODY).asString());
 			}
-			else if("nfm_reply".equals(interactiveType)){
-			    Map<String, Object> replyJsonMap = new HashMap<>();
-			    String responseJsonString = map.entry(InBoundWrapperPaths.INTERACTIVE_NFM_REPLY_RESPONSE_JSON).asString();
-			    replyJsonMap.put("response_json", responseJsonString);
-			    inboxMessage.form().put("reply_json", replyJsonMap);
-			    String body=map.entry(InBoundWrapperPaths.INTERACTIVE_nfm_reply_body).asString();
-			    inboxMessage.form().put("body", body);
-			    inboxMessage.setMessage(ArgUtil.parseAsString(inboxMessage.form().get("body"), Constants.BLANK));
-
-			}
-
+			inboxMessage.setMessage(ArgUtil.parseAsString(inboxMessage.form().get("reply_title"), Constants.BLANK));
 		} else if ("button".equals(messageType)) {
 			inboxMessage.form().put("reply_title", map.entry(InBoundWrapperPaths.SIMPLE_BUTTON_REPLY).asString());
 			String reply_payload = map.entry(InBoundWrapperPaths.SIMPLE_BUTTON_PAYLOAD).asString();
--- boot-jx-parent/lib-postman/src/main/java/com/boot/jx/postman/wa360/WA360Constants.java
+++ boot-jx-parent/lib-postman/src/main/java/com/boot/jx/postman/wa360/WA360Constants.java
@@ -21,10 +21,8 @@ public class WA360Constants {
 		public static final JsonPath INTERACTIVE_BUTTON_REPLY = new JsonPath(
 				"messages/[0]/interactive/button_reply/title");
 		public static final JsonPath INTERACTIVE_BUTTON_ID = new JsonPath("messages/[0]/interactive/button_reply/id");
-		public static final String INTERACTIVE_NFM_REPLY_RESPONSE_JSON = ("messages[0]/interactive/nfm_reply/response_json");
-		public static final String INTERACTIVE_nfm_reply_body = ("messages/[0]/interactive/nfm_resply/body");
-
-
+		public static final String INTERACTIVE_NFM_REPLY_RESPONSE_JSON = ("messages/[0]/interactive/nfm_reply/response_json");
+		public static final String INTERACTIVE_NFM_REPLY_BODY = ("messages/[0]/interactive/nfm_resply/body");
 
 		public static final JsonPath SIMPLE_BUTTON_REPLY = new JsonPath("messages/[0]/button/text");
 		public static final JsonPath SIMPLE_BUTTON_PAYLOAD = new JsonPath("messages/[0]/button/payload");
