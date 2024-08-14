76e228abcf88b6204964e54c24f1790846361408
lalit.tanwar <lalit.tanwar07@gmail.com>
2024-08-14 13:59:34 +0530

    waba options fix

--- boot-jx-parent/common-tmpl/src/test/java/com/amx/tests/HsmTemplateTest.java
+++ boot-jx-parent/common-tmpl/src/test/java/com/amx/tests/HsmTemplateTest.java
@@ -14,21 +14,39 @@ public class HsmTemplateTest {
 	public static CommonTmpPackageImpl ppkg = new CommonTmpPackageImpl();
 
 	public static void main(String[] arg) {
-		new HsmTemplateTest().wa360Templates();
+		new HsmTemplateTest().hsmOptionsCompile();
 	}
 
 	@Test
 	public void wa360Templates() {
 		// assertEquals("t1", StringUtils.trim("/abc/def/ghij", '/'), "abc/def/ghij");
+		MapModel map = sampleModel();
+
+		System.out.println(ppkg.process("   "
+				// + "{{contact.name}} - "
+				// + "{{data.customer}} "
+				+ "{{any data.customer contact.name}} ", map.toMap(), CommonTmpPackageImpl.HANDLEBARS_JS));
+	}
+
+	private MapModel sampleModel() {
 		URL url = FileUtil.getResource("sample/sample_model.json");
 		String json = FileUtil.read(url);
 		MapModel map = MapModel.from(JsonUtil.fromJsonToMap(json));
+		return map;
+	}
 
-		System.out.println(ppkg.process("   "
-				//+ "{{contact.name}} - "
-				//+ "{{data.customer}} "
-				+ "{{any data.customer contact.name}} ",
-				map.toMap(), CommonTmpPackageImpl.HANDLEBARS_JS));
+	private MapModel hsmOptions() {
+		URL url = FileUtil.getResource("sample/hsm_options.json");
+		String json = FileUtil.read(url);
+		MapModel map = MapModel.from(JsonUtil.fromJsonToMap(json));
+		return map;
+	}
+
+	@Test
+	public void hsmOptionsCompile() {
+		// assertEquals("t1", StringUtils.trim("/abc/def/ghij", '/'), "abc/def/ghij");
+		String map = JsonUtil.toJson(hsmOptions());
+		System.out.println(ppkg.process(map, sampleModel().toMap(), CommonTmpPackageImpl.HANDLEBARS_JS));
 	}
 
 }
