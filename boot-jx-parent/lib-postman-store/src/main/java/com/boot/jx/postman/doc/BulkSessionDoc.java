1722636504e62b91e75c6f68fa6db6c443c75ccc
rabiluddin <rabiluddin@gmail.com>
2024-09-06 07:09:39 +0300

    camp summary

--- boot-jx-parent/lib-postman-store/src/main/java/com/boot/jx/postman/doc/BulkSessionDoc.java
+++ boot-jx-parent/lib-postman-store/src/main/java/com/boot/jx/postman/doc/BulkSessionDoc.java
@@ -50,6 +50,10 @@ public class BulkSessionDoc implements AuditCreateEntity, Serializable {
 	private ChronoScheduler scheduler;
 
 	private BatchJob job;
+	
+	private Map<String, Long> campaignSummary;
+
+	
 
 	@Override
 	public String getCreatedBy() {
@@ -224,6 +228,14 @@ public class BulkSessionDoc implements AuditCreateEntity, Serializable {
 
 	public void setScheduler(ChronoScheduler scheduler) {
 		this.scheduler = scheduler;
+	}
+	
+	public Map<String, Long> getCampaignSummary() {
+		return campaignSummary;
+	}
+
+	public void setCampaignSummary(Map<String, Long> campaignSummary) {
+		this.campaignSummary = campaignSummary;
 	}
 
 }
