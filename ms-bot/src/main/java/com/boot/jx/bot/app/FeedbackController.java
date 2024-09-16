0c4230d9962c38cc6726aa1b8c7634f5b1d8d6df
lalit.tanwar <lalit.tanwar07@gmail.com>
2024-09-16 19:14:13 +0530

    Code fromat

--- ms-bot/src/main/java/com/boot/jx/bot/app/FeedbackController.java
+++ ms-bot/src/main/java/com/boot/jx/bot/app/FeedbackController.java
@@ -49,9 +49,8 @@ public class FeedbackController extends CommonBotController {
 	@ChatMapping(key = "feedback-onselect")
 	public void feedback(InboxMessage inboxMessage, StringMatcher matcher) {
 		String text = toReplyEnum(inboxMessage);
-		if(inboxMessage.form().containsKey("reply_id"))
-		{inboxMessage.replyTo().put("type", "feedback");
-		
+		if (inboxMessage.form().containsKey("reply_id")) {
+			inboxMessage.replyTo().put("type", "feedback");
 		}
 
 		if (StringUtils.isNumeric(text)) {
