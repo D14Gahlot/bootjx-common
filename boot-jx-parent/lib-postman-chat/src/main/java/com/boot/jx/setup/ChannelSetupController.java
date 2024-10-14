a9e45694a7cb99171ee5a80a764668b3bee3369c
lalit.tanwar <lalit.tanwar07@gmail.com>
2024-10-15 01:56:08 +0530

    credential

--- boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/setup/ChannelSetupController.java
+++ boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/setup/ChannelSetupController.java
@@ -201,17 +201,16 @@ public class ChannelSetupController {
 	@ApiRequest(tenant = "app")
 	@RequestMapping(value = "/ext/setup/channel/callback/gmail", method = { RequestMethod.GET, RequestMethod.POST })
 	public String setupChannelCallbackGmail(@RequestParam(required = false) String code, Model model,
-			@RequestParam(required = false) String state, @RequestParam(required = false) String token,
+			@RequestParam(required = false) String state, @RequestParam(required = false) String credential,
 			@RequestParam(required = false) String scope)
 			throws FileNotFoundException, IOException, URISyntaxException {
 		AuthState authState = authStateManager.fromState(state);
 		if (ArgUtil.is(authState) && ArgUtil.is(authState.getDomain())) {
 			AppContextUtil.set("domain", authState.getDomain());
 		}
-		model.addAttribute("response",
-				MapModel.createInstance().put(JsonPath.at("authResponse.code"), code)
-						.put(JsonPath.at("authResponse.token"), token).put(JsonPath.at("authResponse.scope"), scope)
-						.put(JsonPath.at("authResponse.state"), state).toJson());
+		model.addAttribute("response", MapModel.createInstance().put(JsonPath.at("authResponse.code"), code)
+				.put(JsonPath.at("authResponse.credential"), credential).put(JsonPath.at("authResponse.scope"), scope)
+				.put(JsonPath.at("authResponse.state"), state).toJson());
 		return this.setupChannel(CHANNEL_TYPE_ENUM.gmail, UniqueID.generateString62(), ContactType.EMAIL,
 				Constants.BLANK, model);
 	}
