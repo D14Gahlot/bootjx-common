bb6c95025e54a77d8e7d9393a5778f47d9d8faac
lalit.tanwar <lalit.tanwar07@gmail.com>
2024-10-29 20:43:00 +0530

    doc updated

--- boot-jx-parent/lib-postman-store/src/main/java/com/boot/jx/postman/service/PMEnvironmentProviderImpl.java
+++ boot-jx-parent/lib-postman-store/src/main/java/com/boot/jx/postman/service/PMEnvironmentProviderImpl.java
@@ -154,6 +154,8 @@ public class PMEnvironmentProviderImpl implements PMEnvironmentProvider, AppShar
 	public ChannelConfig configInternal(ChannelConfig config) {
 		ChannelConfigDoc doc = EntityDtoUtil.dtoToEntity(config, new ChannelConfigDoc());
 		doc.setId(StringUtils.toLowerCase(doc.getChannelId()));
+		ChannelConfigDoc configUpdated = configMaster.findById(config.getChannelId(), ChannelConfigDoc.class);
+		doc.meta().putAll(configUpdated.meta());
 		configMaster.saveChannelConfig(doc);
 		return doc;
 	}
