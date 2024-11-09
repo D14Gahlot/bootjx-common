cc4adab080a25d006dd68c46e18428a69ed1441b
deeksha <gudiy@LAPTOP-N1NJ69G3>
2024-11-09 12:41:30 +0530

    wa360cloud

--- boot-jx-parent/lib-postman/src/main/java/com/boot/jx/postman/wa360/WA360CloudClient.java
+++ boot-jx-parent/lib-postman/src/main/java/com/boot/jx/postman/wa360/WA360CloudClient.java
@@ -349,7 +349,6 @@ public class WA360CloudClient implements ChannelClient {
 				} else if (ArgUtil.is(outboxMessage.getAttachments())) {
 					String lowerFormat = extTemplateComponentFormat.toLowerCase();
 					WA360CloudOutBoundMedia media = createMedia(lowerFormat, outboxMessage.getAttachments().get(0));
-			        media.setFilename(ArgUtil.nonEmpty(outboxMessage.getAttachments().get(0).getMediaCaption(), outboxMessage.getAttachments().get(0).getMediaName()));
 					media.setCaption(null);
 					headerComponentReq.parameter(lowerFormat, media);
 					if (headerComponentReq.parameters().size() > 0) {
