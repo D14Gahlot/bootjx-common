a2ba595f0b7e175cb9dc85bb7d8ebe540a084ca8
lalit.tanwar <lalit.tanwar07@gmail.com>
2025-01-25 14:21:13 +0530

    bulk model update

--- boot-jx-parent/lib-mcq/src/main/java/com/boot/jx/tunnel/task/BatchJobExecuter.java
+++ boot-jx-parent/lib-mcq/src/main/java/com/boot/jx/tunnel/task/BatchJobExecuter.java
@@ -41,7 +41,7 @@ public abstract class BatchJobExecuter {
 
 	private String getJobName() {
 		if (this.jobName == null) {
-			this.jobName = ClazzUtil.getUltimateClassName(this) + "V9";
+			this.jobName = ClazzUtil.getUltimateClassName(this) + "V10";
 		}
 		return this.jobName;
 	}
