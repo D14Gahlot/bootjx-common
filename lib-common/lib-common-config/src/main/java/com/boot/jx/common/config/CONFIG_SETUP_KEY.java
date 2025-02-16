dd9bd2d153b6d00999b220f69a5fd4ec0b7a0809
lalit.tanwar <lalit.tanwar07@gmail.com>
2025-02-16 16:46:41 +0530

    added messag tracker

--- lib-common/lib-common-config/src/main/java/com/boot/jx/common/config/CONFIG_SETUP_KEY.java
+++ lib-common/lib-common-config/src/main/java/com/boot/jx/common/config/CONFIG_SETUP_KEY.java
@@ -45,6 +45,11 @@ public enum CONFIG_SETUP_KEY implements EntryMeta {
 			.desc("This channel will be connected your Page").optionsSource("getx:/api/options/channels")
 			.optionsKey("id").optionsLabel("name").filter("contactType", ContactType.WEBSITE)),
 
+	POSTMAN_TRACK_MESSAGE(new ConfigMeta("Message Tracker", "postman.track.message").superKey("postman.track.message")
+			.desc("Use Mehery's Tracker to track status for Channels (with no Status support)").optionsOnOff()),
+
+	POSTMAN_TRACK_MESSAGE_URL(new ConfigMeta("Message Tracker", "postman.track.message.url").desc("Tracker URL")),
+
 	POSTMAN_AGENT_2FA_ENABLED(new ConfigMeta("Enable 2FA Login", PROPERTIES.POSTMAN_AGENT_2FA_ENABLED)
 			.superKey(PROPERTIES.POSTMAN_AGENT_2FA_ENABLED).desc("You will need OA app").optionsOnOff()),
 
@@ -219,21 +224,17 @@ public enum CONFIG_SETUP_KEY implements EntryMeta {
 			PROPERTIES.POSTMAN_AGENT_CHAT_AUTOREPLY_RESOLVED).optionsSource("getx:/api/tmpl/hsm").optionsKey("code")
 					.optionsLabel("desc").group(ConfigConstants.GROUP_AGENT)),
 
-
-	POSTMAN_AGENT_CHAT_AUTOREPLY_NOAGENT(
-			new ConfigMeta("Message to customer when no agent available in working hours", "postman.agent.chat.autoreply.agent_notfound")
-					.optionsSource("getx:/api/tmpl/hsm").optionsKey("code").optionsLabel("desc")
-					.group(ConfigConstants.GROUP_AGENT)),
+	POSTMAN_AGENT_CHAT_AUTOREPLY_NOAGENT(new ConfigMeta("Message to customer when no agent available in working hours",
+			"postman.agent.chat.autoreply.agent_notfound").optionsSource("getx:/api/tmpl/hsm").optionsKey("code")
+					.optionsLabel("desc").group(ConfigConstants.GROUP_AGENT)),
 
 	POSTMAN_AGENT_CHAT_AUTOREPLY_ORGOFFLINE(
 
-			new ConfigMeta("Message to customer when no agent available in non-working hours", "postman.agent.chat.autoreply.org_offline")
-					.optionsSource("getx:/api/tmpl/hsm").optionsKey("code").optionsLabel("desc")
-					.group(ConfigConstants.GROUP_AGENT)),
-	POSTMAN_AGENT_CHAT_SCHEDULE(
-			new ConfigMeta("List of schedules", "postman.agent.chat.schedule")
-			.optionsSource("getx:/nexus/calendar/api/v1/orgSchedule/list")
-			.optionsKey("code").optionsLabel("desc")
+			new ConfigMeta("Message to customer when no agent available in non-working hours",
+					"postman.agent.chat.autoreply.org_offline").optionsSource("getx:/api/tmpl/hsm").optionsKey("code")
+							.optionsLabel("desc").group(ConfigConstants.GROUP_AGENT)),
+	POSTMAN_AGENT_CHAT_SCHEDULE(new ConfigMeta("List of schedules", "postman.agent.chat.schedule")
+			.optionsSource("getx:/nexus/calendar/api/v1/orgSchedule/list").optionsKey("code").optionsLabel("desc")
 
 			.group(ConfigConstants.GROUP_AGENT)),
 
