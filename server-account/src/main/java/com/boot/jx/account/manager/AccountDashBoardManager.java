dfe35f2cdc080f1ace4fd2eb9d894b434a77bd9d
rabiluddin <rabiluddin@gmail.com>
2025-03-05 08:26:24 +0530

    index error

--- server-account/src/main/java/com/boot/jx/account/manager/AccountDashBoardManager.java
+++ server-account/src/main/java/com/boot/jx/account/manager/AccountDashBoardManager.java
@@ -1164,11 +1164,11 @@ public class AccountDashBoardManager {
 			String[] contactids = contactid.split("_");
 			if (contactids != null && contactids.length>1 && contactids[1] != null) {
 				lane = contactids[1];
-			}
-		}else if(ArgUtil.is(contactid)) {
-			String[] contactids = contactid.split(":");
-			if (contactids != null && contactids.length > 1) {
-				lane = contactids[1];
+			}else {
+				contactids = contactid.split(":");
+				if (contactids != null && contactids.length > 1) {
+					lane = contactids[1];
+				}
 			}
 		}
 		return lane;
