1319d6030cb1e4bfdc0e58e6d5c3ad1d61ff75e3
rabiluddin <rabiluddin@gmail.com>
2025-03-05 08:26:24 +0530

    index error

--- server-admin/src/main/java/com/boot/jx/admin/manager/AdminDashBoardManager.java
+++ server-admin/src/main/java/com/boot/jx/admin/manager/AdminDashBoardManager.java
@@ -1867,11 +1867,11 @@ public class AdminDashBoardManager {
 			String[] contactids = contactid.split("_");
 			if (contactids != null && contactids.length > 1) {
 				lane = contactids[1];
-			}
-		}else if(ArgUtil.is(contactid)) {
-			String[] contactids = contactid.split(":");
-			if (contactids != null && contactids.length > 1) {
-				lane = contactids[1];
+			}else {
+				contactids = contactid.split(":");
+				if (contactids != null && contactids.length > 1) {
+					lane = contactids[1];
+				}
 			}
 		}
 		return lane;
