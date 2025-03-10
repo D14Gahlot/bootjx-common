8a7dc978286b2030167e623fe02620d8202213d6
lalit.tanwar <lalit.tanwar07@gmail.com>
2025-03-10 18:41:59 +0530

    sesison close batch support

--- boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/session/SessionEventController.java
+++ boot-jx-parent/lib-postman-chat/src/main/java/com/boot/jx/session/SessionEventController.java
@@ -1,5 +1,8 @@
 package com.boot.jx.session;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
@@ -15,6 +18,7 @@ import com.boot.jx.api.ApiResponse;
 import com.boot.jx.chat.ChatSessionService;
 import com.boot.jx.postman.model.ext.InBoundEvent;
 import com.boot.jx.postman.model.ext.SessionBoundEvent;
+import com.boot.utils.ArgUtil;
 
 @RestController
 @RequestMapping("/events")
@@ -28,7 +32,15 @@ public class SessionEventController {
 	@RequestMapping(value = "/api/v1/session/status/close", method = RequestMethod.POST)
 	public ApiResponse<InBoundEvent, Object> closeSessionBySessionId(Model model, HttpServletRequest request,
 			@RequestBody SessionBoundEvent event) throws InterruptedException {
-		return ApiResponse.buildResult(chatSessionService.closeSession(event.getSessionId()).value());
+		List<InBoundEvent> results = new ArrayList<InBoundEvent>();
+		if (ArgUtil.is(event.getSessionId())) {
+			results.add(chatSessionService.closeSession(event.getSessionId()).value());
+		} else if (ArgUtil.is(event.getSessionIds())) {
+			for (String sessionid : event.getSessionIds()) {
+				results.add(chatSessionService.closeSession(sessionid).value());
+			}
+		}
+		return ApiResponse.buildResults(results);
 	}
 
 }
